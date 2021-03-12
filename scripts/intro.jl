e1630304849b3b3d3c0809903350aefaa8fced94
DrWatson <no@mail>
2021-03-12 15:57:48 -0600

    Paquete inicial y version DrWatson

---
+++ scripts/intro.jl
@@ -0,0 +1,14 @@
+using DrWatson
+@quickactivate "HEMI"
+DrWatson.greet()
+
+# Script de lectura de datos de CSV 
+# Trabajar en los tipos para representar los datos
+# Funciones básicas de índices y variaciones
+# Función básica para variación interanual del IPC con varias bases
+# ... (mucho más)
+
+## 
+using CPIDataBase
+CPIDataBase.f(2)
+CPIDataBase.g(2) # Probando Revise
--- src/CPIDataBase/src/CPIDataBase.jl
+++ src/CPIDataBase/src/CPIDataBase.jl
@@ -1,5 +1,17 @@
+"""
+    CPIDataBase
+
+    Librería base para tipos y funcionalidad básica para manejo de datos del IPC a nivel desagregado de gastos básicos
+"""
 module CPIDataBase
 
 greet() = print("Hello World!")
+f(x) = 2x+1
+g(x) = 2x^2+2
+
+# export f
+
+include("types.jl")
+export CPIBase
 
 end # module
