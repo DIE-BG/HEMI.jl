d6ab76637c38960a0736d96ab54d9308e5de8568
Rodrigo Chang <rrcp777@gmail.com>
2021-06-04 12:21:59 -0600

    Update media_simple.jl
    Agregamos documentación a tipos y funciones

--- src/InflationFunctions/src/media_simple.jl
+++ src/InflationFunctions/src/media_simple.jl
@@ -1,11 +1,18 @@
 # media_simple.jl - Función de inflación de media media_simple
 
 # 1. Definir un tipo
+"""
+    MediaSimple <: InflationFunction
+Función de inflación para compuar la media simple.
+"""
 Base.@kwdef struct MediaSimple <: InflationFunction
     name::String = "Media simple"
 end
 
-# 2. Definir cómo opera MediaSimple sobre un objeto de tipo VarCPIBase
+"""
+    (inflfn::MediaSimple)(base::VarCPIBase)
+Define cómo opera MediaSimple sobre un objeto de tipo VarCPIBase.
+"""
 function (inflfn::MediaSimple)(base::VarCPIBase)
     mean(base.v, dims=2)
 end
