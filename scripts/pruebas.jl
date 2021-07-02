2130379c65b0a4863ba657047afa6fadab451638
Marianojgm86 <59839138+Marianojgm86@users.noreply.github.com>
2021-07-02 12:02:27 -0600

    Ampliación de algoritmo
    - Ampliación de algoritmo de cálculo de función de exclusión fija.

--- scripts/pruebas.jl
+++ scripts/pruebas.jl
@@ -17,4 +17,22 @@ for i in list_exc w_exc[i] = 0 end
 w_exc = w_exc / sum(w_exc)
 
 
-##########
+## ########
+
+v_exc = ([35,30,190,36,37,40,31,104,162,32,33,159,193,161,218],[25, 40, 45, 50, 55, 70, 75, 80, 85, 275, 279])
+base_ipc = (Matrix{Float32}, Matrix{Float32})
+w_exc = (Vector{Float32}, Vector{Float32})
+for i in 1:length(gtdata.base)
+    # Capitalizar los índices de precios a partir del objeto cs.VarCPIBase[i]
+    base_ipc = capitalize(gtdata.base[i].v, gtdata.base[i].baseindex)
+    # Copia de la lista original de pesos desde cs.base[i]
+    w_exc = copy(gtdata.base[i].w)
+    # Asignación de peso cero a los gastos básicos de la lista de exclusión (v_exc[i]) (j itera sobre los elementos de la lista de exclusión)
+        for j in v_exc[i] w_exc[j] = 0.0 end
+    # Renormalización de pesos
+    w_exc = w_exc / sum(w_exc)
+    # Obtener IPC
+    cpi_exc = sum(base_ipc.*w_exc', dims=2)
+    # Obtener variación intermensual
+    varinterm(cpi_exc)
+end
--- src/InflationFunctions/src/InflationFixedExclusionCPI.jl
+++ src/InflationFunctions/src/InflationFixedExclusionCPI.jl
@@ -6,39 +6,47 @@
 Función de inflación para computar la inflación de exclusión fija de gastos básicos.
 """
 Base.@kwdef struct InflationFixedExclusionCPI <: InflationFunction
-    # Dos vectores, uno por base
-    f_exc::Vector{Int64} # Vector con Gastos básicos a excluir (que tipo, vector de posiciones o vector de códigos de gb???)
+    # Tupla con vectores de gastos básicos a exlcuir en cada base 
+    # (deberá tener la misma cantidad de vectores que bases en el CountryStructure)
+    v_exc::NTuple{2,Vector{Int64}} 
+
 end
 
 # 2. Extender el método de nombre 
 measure_name(::InflationFixedExclusionCPI) = "Inflación de Exclusión Fija de Gastos Básicos"
 
 """
-    (inflfn::InflationFixedExclusionCPI)(base::VarCPIBase)
-Define cómo opera InflationFixedExclusionCPI sobre un objeto de tipo VarCPIBase.
+    (inflfn::InflationFixedExclusionCPI)(base::UniformCountryStructure, v_exc)
+Define cómo opera InflationFixedExclusionCPI sobre un objeto de tipo CountryStructure, 
+con listas de exclusión para las bases 2000 y 2010.
+PROBLEMAS: los objetos base_ipc, w_exc, cpi_exc y varm_cpi_exc ¿debieran ser tuplas?, ¿Debó crearlas antes?
 """
-function (inflfn::InflationFixedExclusionCPI)(base::VarCPIBase, f_exc)
-    # Capitalizar los índices de precios a partir del objeto VarCPIBase
-    base_ipc = capitalize(base.v, base.baseindex)
-    # Asignación de peso cero a los gastos básicos de la lista de exclusión (f_exc)
-    w_exc = copy(base.w)
-    for i in inflfn.f_exc w_exc[i] = 0 end
+function (inflfn::InflationFixedExclusionCPI)(cs::UniformCountryStructure, v_exc)
+    # Iteración sobre la cantidad de bases en cs 
+    for i in 1:length(cs.base)
+    # Capitalizar los índices de precios a partir del objeto cs.VarCPIBase[i]
+    base_ipc[i] = capitalize(cs.base[i].v, cs.base[i].baseindex)
+    # Copia de la lista original de pesos desde cs.base[i]
+    w_exc[i] = copy(cs.base[i].w)
+    # Asignación de peso cero a los gastos básicos de la lista de exclusión (v_exc[i]) 
+    # (j itera sobre los elementos de la lista de exclusión)
+        for j in inflfn.v_exc[i] w_exc[i][j] = 0.0 end
     # Renormalización de pesos
-    w_exc = w_exc / sum(w_exc)
-    # Obtener Ipc 
-        # Obtener el IPC del mes de manera normal (y ¿aplicar función de inflación total?). cpi_exc = cpi_mat * w_exc
-        #sum(gt10.v, dims=2)
-        ## DEVOLVER VARIACIONES INTERMENSUALES E INDICES CONCATENADOS PARA AMBAS BASES
-
-end 
-
+    w_exc[i] = w_exc[i] / sum(w_exc[i])
+    # Obtener Ipc con exclusión 
+    cpi_exc[i] = sum(base_ipc[i].*w_exc[i]', dims=2)
+    # Obtener variación intermensual
+    varm_cpi_exc[i] =  varinterm(cpi_exc[i])
+    end
+ varm_cpi_exc 
+end
 ## PARA DEFINIR COMO OPERA LA FUNCIÓN DE INFLACIÓN SOBRE COUNTRYSTRUCTURE 
 # OJO AQUI
 
 # Aplicar cada lista de exclusión con su base correspondiente en el CountryStructure
-function (inflfn::InflationFixedExclusionCPI)(cs::CountryStructure, ::CPIVarInterm) 
-    # Acá se llama a inflfn(base), en donde base es de tipo VarCPIBase. Esta
-    # es la función que debe definirse para cualquier medida de inflación.
-    vm = mapfoldl(inflfn, vcat, cs.base)
-    vm
-end
+# function (inflfn::InflationFixedExclusionCPI)(cs::CountryStructure, ::CPIVarInterm) 
+#     # Acá se llama a inflfn(base), en donde base es de tipo VarCPIBase. Esta
+#     # es la función que debe definirse para cualquier medida de inflación.
+#     vm = mapfoldl(inflfn, vcat, cs.base)
+#     vm
+# end
