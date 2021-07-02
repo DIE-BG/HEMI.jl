10d7a593a21bb7a80549870fe46924a181d1385d
Rodrigo Chang <rrcp777@gmail.com>
2021-07-02 17:20:25 -0600

    Cambios en docstrings

--- src/InflationFunctions/src/InflationTrimmedMeanEq.jl
+++ src/InflationFunctions/src/InflationTrimmedMeanEq.jl
@@ -13,35 +13,41 @@ end
     InflationTrimmedMeanEq(l1::Real,l2::Real)
 Nos permite utilizar valores que no necesariamente son Float32, como enteros o Float64.
 
-ejemplo: InflationTrimmedMeanEq(25,75.5)
+# Ejemplo: 
+```julia-repl
+julia> mtfn = InflationTrimmedMeanEq(25,75.5)
+(::InflationTrimmedMeanEq) (generic function with 5 methods)
+```
 """
-InflationTrimmedMeanEq(l1::Real,l2::Real) = InflationTrimmedMeanEq(l1 = Float32(l1), l2 = Float32(l2))
+InflationTrimmedMeanEq(l1::Real,l2::Real) = InflationTrimmedMeanEq(l1=Float32(l1), l2=Float32(l2))
 
 """
     measure_name(inflfn::InflationTrimmedMeanEq)
 
-Nos indica que medida se utiliza para una instancia.
+Nos indica qué medida se utiliza para una instancia de una función de inflación.
 
-ejemplo:  
+# Ejemplo:  
 
-julia> instancia = InflationTrimmedMeanEq(15.5,75.5); \\
-julia> measure_name(instancia) \\
+```julia-repl
+julia> mtfn = InflationTrimmedMeanEq(15.5,75.5)
+julia> measure_name(mtfn) 
 "Media Truncada Equiponderada (15.5 , 75.5)"
+```
 """
-measure_name(inflfn::InflationTrimmedMeanEq) = "Media Truncada Equiponderada (" * string(round(inflfn.l1, digits=2)) * " , " * string(round(inflfn.l2, digits=2)) * ")"
+measure_name(inflfn::InflationTrimmedMeanEq) = "Media Truncada Equiponderada (" * string(round(inflfn.l1, digits=2)) * ", " * string(round(inflfn.l2, digits=2)) * ")"
 
 """
-    (inflfn::InflationTrimmedMeanEq)(base::VarCPIBase{T}) where T
+    function (inflfn::InflationTrimmedMeanEq)(base::VarCPIBase{T}) where T
 Define cómo opera InflationTrimmedMeanEq sobre un objeto de tipo VarCPIBase.
 """
 function (inflfn::InflationTrimmedMeanEq)(base::VarCPIBase{T}) where T 
     l1 = inflfn.l1
     l2 = inflfn.l2                                          
-    leftPercentile,rightPercentile = min(l1,l2),max(l1,l2)   
+    leftPercentile, rightPercentile = min(l1, l2), max(l1, l2)   
     # determinanmos en donde tuncar                                 
-    q1    = Int(ceil(length(base.w)*leftPercentile/100))                        
-    q2    = Int(floor(length(base.w)*rightPercentile/100))                       
-    outVec   = Vector{T}(undef,size(base.v)[1]) 
+    q1    = Int(ceil(length(base.w) * leftPercentile / 100))                        
+    q2    = Int(floor(length(base.w) * rightPercentile / 100))                       
+    outVec   = Vector{T}(undef, size(base.v)[1]) 
     # para cada t: ordenamos, truncamos y obtenemos la media.                      
     for i in 1:size(base.v)[1]                                      
         temporal    = base.v[i,:]                                      
--- src/InflationFunctions/src/InflationTrimmedMeanWeighted.jl
+++ src/InflationFunctions/src/InflationTrimmedMeanWeighted.jl
@@ -12,26 +12,31 @@ end
     InflationTrimmedMeanWeighted(l1::Real,l2::Real)
 Nos permite utilizar valores que no necesariamente son Float32, como enteros o Float64.
 
-ejemplo: InflationTrimmedMeanWeighted(25,75.5)
+# Ejemplo: 
+```julia-repl
+julia> mtfn = InflationTrimmedMeanWeighted(25,75.5)
+(::InflationTrimmedMeanWeighted) (generic function with 5 methods)
+```
 """
 InflationTrimmedMeanWeighted(l1::Real,l2::Real) = InflationTrimmedMeanWeighted(l1 = Float32(l1), l2 = Float32(l2))
 
 """
     measure_name(inflfn::InflationTrimmedMeanWeighted)
 
-Nos indica que medida se utiliza para una instancia.
+Nos indica que medida se utiliza para una instancia de una función de inflación.
 
-ejemplo:  
-
-julia> instancia = InflationTrimmedMeanWeighted(15.5,75.5); \\
-julia> measure_name(instancia) \\
+# Ejemplo:  
+```julia-repl
+julia> mtfn = InflationTrimmedMeanWeighted(15.5,75.5)
+julia> measure_name(mtfn)
 "Media Truncada Ponderada (15.5 , 75.5)"
+```
 """
 measure_name(inflfn::InflationTrimmedMeanWeighted) = "Media Truncada Ponderada (" * string(round(inflfn.l1, digits=2)) * " , " * string(round(inflfn.l2, digits=2)) * ")"
 
 
 """
-    (inflfn::InflationTrimmedMeanWeighted)(base::VarCPIBase{T}) where T
+    function (inflfn::InflationTrimmedMeanWeighted)(base::VarCPIBase{T}) where T
 Define cómo opera InflationTrimmedMeanWeighted sobre un objeto de tipo VarCPIBase.
 """
 function (inflfn::InflationTrimmedMeanWeighted)(base::VarCPIBase{T}) where T     
