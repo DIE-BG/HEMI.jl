a9eeeae1e406caca29789e552f2c74afa41cd5c5
Rodrigo Chang <rrcp777@gmail.com>
2021-07-06 17:48:26 -0600

    Documentación parcial medidas de inflación

--- src/CPIDataBase/src/inflation/inflation.jl
+++ src/CPIDataBase/src/inflation/inflation.jl
@@ -1,23 +1,54 @@
 # inflation.jl - Estructura básica para cómputo de estimadores de inflación interanual
 
+"""
+    abstract type InflationFunction <: Function
+
+Tipo abstracto para representar las funciones de inflación que operan sobre
+[`CountryStructure`](@ref) y [`VarCPIBase`](@ref). Permiten computar la medida
+de ritmo inflacionario interanual, el índice de precios dado por la metodología
+y las variaciones intermensuales del índice de precios.
+"""
 abstract type InflationFunction <: Function end
 
 # La mayoría de funciones de inflación devuelven una medida
+"""
+    num_measures(::InflationFunction)
+
+Devuelve la cantidad de medidas devueltas por la función de inflación. Las
+funciones de [`EnsembleFunction`](@ref) pueden computar varias medidas de
+inflación simultáneamente.
+"""
 num_measures(::InflationFunction) = 1
 
 # Funciones de inflación deben extender el método measure_name para devolver su nombre
+"""
+    measure_name(inflfn::InflationFunction)
+
+Este método permite obtener el nombre convencional de una medida de inflación. 
+"""
 measure_name(inflfn::InflationFunction) = 
     error("Se debe extender el método `measure_name` para la función de inflación " * string(nameof(inflfn)))
     
 # Funciones de inflación pueden extender opcionalmente un método measure_tag
 # para indicar en archivos de resultados. Si no, se tomará por defecto el nombre
 # del tipo de la función de inflación
+"""
+    measure_tag(inflfn::InflationFunction)
+
+Obtiene una etiqueta de la medida de inflación. Se puede utilizar para guardar
+como parámetro en archivos de resultados de evaluación.
+"""
 measure_tag(inflfn::InflationFunction) = string(nameof(inflfn))
 
 
 ## Tipos para resultados, utilizados para el despacho de métodos
+"Tipo abstracto para manejar el despacho de las funciones de inflación"
 abstract type CPIResult end
+"Tipo concreto único para obtener el índice de una función de inflación"
 struct CPIIndex <: CPIResult end
+"""
+Tipo concreto único para obtener la variación intermensual de una función de inflación
+"""
 struct CPIVarInterm <: CPIResult end
 
 ## Esquema general de cómputo (programación genérica con tipos abstractos): 
