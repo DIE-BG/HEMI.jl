07306d4b951c9581ef8921a9c14619b96c307d8b
djgabrielm <dgabrielmaselli@gmail.com>
2021-07-07 08:34:29 -0600

    Update InflationTrimmedMeanWeighted.jl
    Una mejora marginal a InflationTrimmedMeanWeighted

--- src/InflationFunctions/src/InflationTrimmedMeanWeighted.jl
+++ src/InflationFunctions/src/InflationTrimmedMeanWeighted.jl
@@ -41,23 +41,22 @@ measure_name(inflfn::InflationTrimmedMeanWeighted) = "Media Truncada Ponderada (
 
 function (inflfn::InflationTrimmedMeanWeighted)(base::VarCPIBase{T}) where T     
     l1 = inflfn.l1
-    l2 = inflfn.l2                                        
-    leftPercentile,rightPercentile = min(l1,l2),max(l1,l2)                                   
+    l2 = inflfn.l2                                                                          
     # determinamos donde truncar
-    q1    = Int(ceil(length(base.w)*leftPercentile/100))                       
-    q2    = Int(floor(length(base.w)*rightPercentile/100))                       
-    outVec   = Vector{T}(undef,size(base.v)[1])                         
+    q1    = Int(ceil(length(base.w)*min(l1,l2)/100))                       
+    q2    = Int(floor(length(base.w)*max(l1,l2)/100))                       
+    outVec   = Vector{T}(undef,periods(base))                         
     # para cada t: creamos parejas de variaciones con pesos,
     # ordenamos de acuerdo a variaciones, truncamos
     # renormalizamos para que los pesos sumen 1
     # sumamos el producto de variaciones con pesos
-    for i in 1:size(base.v)[1]                                      
-        temporal        = hcat(base.v[i,:],base.w)                      
+    for i in 1:periods(base)                                     
+        temporal        = hcat((@view base.v[i,:]),base.w)                      
         temporal        = temporal[sortperm(temporal[:,1]),:]                    
-        temporal        = temporal[q1:q2,:]                                 
+        temporal        = @view temporal[q1:q2,:]                                 
         temporal[:,2]   = temporal[:,2] .* 1/sum(temporal[:,2])                 
-        outVec[i]      = sum(temporal[:,1] .* temporal[:,2])                   
+        outVec[i]       = sum(temporal[:,1] .* temporal[:,2])                   
     end
     return outVec
 end 
- 
+
