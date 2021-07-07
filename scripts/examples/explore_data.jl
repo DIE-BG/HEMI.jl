11be0d73aed2dc8adc5eef447681d988c4e885e6
Rodrigo Chang <rrcp777@gmail.com>
2021-07-07 10:05:29 -0600

    Pruebas en despliegue

--- scripts/examples/explore_data.jl
+++ scripts/examples/explore_data.jl
@@ -24,3 +24,23 @@ propertynames(gt10)
 
 # Ahora utilizamos el contenedor `UniformCountryStructure`, el cual contiene una tupla de objetos `VarCPIBase`: 
 gtdata
+
+# ## Obteniendo datos en direferentes rangos
+# Esta estructura de país puede ser indexada para obtener las bases de tipo
+# `VarCPIBase` que contiene. Por ejemplo: 
+gtdata[1]
+
+# También puede ser indexada por la fecha inicial y final, o bien, únicamente por la fecha final: 
+gtdata[Date(2005, 1), Date(2019, 12)]
+#-
+gtdata[Date(2019, 12)]
+
+# Al indexar de esta manera se crea una copia del `UniformCountryStructure`.
+
+
+# ## Funciones de utilidad
+# Para obtener el número de períodos de una base de tipo `VarCPIBase` utilizamos la función `periods`: 
+periods(gt00)
+
+# De igual forma, este método aplica para un `CountryStructure`:
+periods(gtdata)
