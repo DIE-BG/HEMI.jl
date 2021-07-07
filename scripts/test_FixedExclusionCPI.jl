7b53314821fc5bfee6b66697d42785ab81fa41ec
Marianojgm86 <59839138+Marianojgm86@users.noreply.github.com>
2021-07-07 12:41:59 -0600

    Update test_FixedExclusionCPI.jl
    Prueba en test_FixedExclusionCPI con vectores de exclusión óptimos para Evaluación 2020. Coincidencia de los resultados con MATLAB al cuarto decimal.

--- scripts/test_FixedExclusionCPI.jl
+++ scripts/test_FixedExclusionCPI.jl
@@ -8,11 +8,16 @@ using BenchmarkTools
 
 
 ## 
+# Vectores de exclusión de prueba
 v_exc00 = [35,30,190,36,37,40,31,104,162,32,33,159,193,161,218]
 v_exc10 = [25, 40, 45, 50, 55, 70, 75, 80, 85, 275, 279]
 
-##
-fxEx = InflationFixedExclusionCPI(v_exc00, v_exc10)
+# Vectores de exclsión óptimos según Evaluación 2020.
+excOpt00 = [35,30,190,36,37,40,31,104,162,32,33,159,193,161]
+excOpt10 = [29,31,116,39,46,40,30,35,186,47,197,41,22,48,185,34,184]
+
+## Prueba para exclusió óptima 2020
+fxEx = InflationFixedExclusionCPI(excOpt00, excOpt10)
 fxEx(gt10,2)
 fxEx(gt00,1)
 fxEx(gtdata)
