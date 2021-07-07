e2f7b85ab63ea36769bd145998b57ebe9a82618b
Marianojgm86 <59839138+Marianojgm86@users.noreply.github.com>
2021-07-07 14:23:53 -0600

    Update test_FixedExclusionCPI.jl
    Se agregaron las pruebas para las medidas de exclusión:
    - Exclusión óptima DIE
    - Exclusión fija de Alimentos y Energéticos
    - Exclusión fija de Energéticos

--- scripts/test_FixedExclusionCPI.jl
+++ scripts/test_FixedExclusionCPI.jl
@@ -16,8 +16,16 @@ v_exc10 = [25, 40, 45, 50, 55, 70, 75, 80, 85, 275, 279]
 excOpt00 = [35,30,190,36,37,40,31,104,162,32,33,159,193,161]
 excOpt10 = [29,31,116,39,46,40,30,35,186,47,197,41,22,48,185,34,184]
 
+# Exclusión Alimentos y Energéticos
+AEExc_00 = vcat(collect(23:41), 104, 159)
+AEExc_10 = vcat(collect(22:48), 116, collect(184:186))
+
+## Exclusión solo energéticos
+EExc_00 = [104, 159]
+EExc_10 = vcat(116, collect(184:186))
+
 ## Prueba para exclusió óptima 2020
-fxEx = InflationFixedExclusionCPI(excOpt00, excOpt10)
+fxEx = InflationFixedExclusionCPI(AEExc_00, AEExc_10)
 fxEx(gt10,2)
 fxEx(gt00,1)
 fxEx(gtdata)
