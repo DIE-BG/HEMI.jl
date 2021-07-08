01a25f84548875401ec789ad7c13acb89a413775
Marianojgm86 <59839138+Marianojgm86@users.noreply.github.com>
2021-07-08 11:29:10 -0600

    Exclusión fija de gastos Básicos #20 (#25)
    * Commit inicial para Exclusión Fija
    
    Commit inicial para creación de pull-request
    
    * Delete xlsx
    
    * creación de InflationFixedExclusionCPI
    
    Creación de función de inflación para inflación de exclusión fija. Estructura general.
    
    * HEMI-showcase al gitignore
    
    * Creación de pruebas.jl
    
    Script de pruebas base para creación de función
    
    * Update pruebas.jl
    
    Prueba utilizando country structure (gtdata)
    
    * Ajuste en Manifiest.toml
    
    - Ajuste en manifiesto por paquete DependentBootstrap.jl.
    
    - Utilización de objeto VarCPIBase para cálculos. Se capitalizan los índices en la función.
    
    * Avances 01/07/02021
    
    - Ampliación de método de exclusión fija en InflationFunctions.jl
    - Ampliación de InflationFixedExclusion.jl. Falta ampliar para ambas bases.
    
    * Instrucción
    
    * Ampliación de algoritmo
    
    - Ampliación de algoritmo de cálculo de función de exclusión fija.
    
    * Pruebas con InflationFixedExclutionCPI
    
    - Pruebas con objetos dentro de la función
    
    * Update InflationFixedExclusionCPI.jl
    
    - Función genera una tupla con dos "vectores" de elementos float32, con las variaciones intermensuales, con 120 y 122 períodos respectivamente.
    
    * Update InflationFixedExclusionCPI.jl
    
    * Create InflationFixedExclusionCPIB.jl
    
    Función que opera sobre objeto VarCPIBase
    
    * Pruebas con función
    
    Creación de una función para objeto VarCPIBase y otra para CountryStructure
    
    * Corrección de función de inflación
    
    Se corrigió la función de inflación.
    Recibe una tupla de dos vectores de exlcusión y dependiendo del tamaño del elemento base.v, elige el elemento de la tupla para utilizar.
    Falta mejorar el método para la aplicación al  CountryStructure
    
    * Corrección docstring
    
    * Update InflationFixedExclusionCPI.jl
    
    Reubicación de docstrings
    
    * Ajustes según reunión 06/07/2021
    
    Se realizaron los cambios recomendados por @r2cp en la reunión en mención.
    - Se definió la función para que tome como insumo la posición del vector de exclusión en v_exc (i).
    - Se realizaron los ajustes necesario para redefinir el comprtamiento de la variable al recibir un CountryStructure.
    - Se agregaron comentarios y ejemplos en los docstrings.
    
    Adicionalmente se realizaron algunas pruebas en pruebas.jl.
    
    Se agregaron las pruebas (sin cambios) en runtest.jl para la función FixedExclusionCPI. Hay algunas observaciones en este sentido (el primer elemento de los cálculos para los zerobases no es cero, por lo que el test falla).
    
    * Commit reunión vespertina 06/07/2021
    
    Ajuste de test para InflationFixedExclusionCPI
    
    * creación test_FixedExclutionCPI
    
    * Update test_FixedExclusionCPI.jl
    
    Prueba en test_FixedExclusionCPI con vectores de exclusión óptimos para Evaluación 2020. Coincidencia de los resultados con MATLAB al cuarto decimal.
    
    * Update test_FixedExclusionCPI.jl
    
    Se agregaron las pruebas para las medidas de exclusión:
    - Exclusión óptima DIE
    - Exclusión fija de Alimentos y Energéticos
    - Exclusión fija de Energéticos
    
    * Update test_FixedExclusionCPI.jl
    
    * Mejoras en docstring
    
    El docstring no se mostraba al pedir ayuda. Se corrigió quitando el comentario arriba de la definición.
    
    Co-authored-by: r2cp <5157100+r2cp@users.noreply.github.com>
    Co-authored-by: Rodrigo Chang <rrcp777@gmail.com>

---
+++ src/InflationFunctions/src/InflationFixedExclusionCPI.jl
@@ -0,0 +1,107 @@
+# ## InflationFixedExclusionCPI.jl - Función de inflación de exclusión fija de gastos básicos
+
+
+"""
+    InflationFixedExclusionCPI{N} <: InflationFunction
+
+Función de inflación para computar la inflación de exclusión fija de gastos
+básicos para `N` bases del IPC.
+
+## Utilización
+
+    function (inflfn::InflationFixedExclusionCPI)(base::VarCPIBase{T}, i::Int) where T 
+
+Define cómo opera InflationFixedExclusionCPI sobre un objeto de tipo
+`VarCPIBase`, con `i` listas de exclusión de gastos básicos (posiciones en el
+IPC) para las bases 2000 y 2010.
+
+    function (inflfn::InflationFixedExclusionCPI)(cs::CountryStructure, ::CPIVarInterm) 
+
+Define cómo opera InflationFixedExclusionCPI sobre un CountryStructure
+
+## Ejemplos
+
+Instanciamos la función y le damos dos listas de exclusión, una por cada base en
+el CountryStructure. Se exclyen los gastos básicos con números de columnas
+`[25,30,54,88]` del primer `VarCPIBase` y los gastos básicos `[65,95,85]` del
+segundo `VarCPIBase`.
+```julia-repl 
+julia> InfExc = InflationFixedExclusionCPI([25,30,54,88], [65,95,85]) 
+(::InflationFixedExclusionCPI{2}) (generic function with 6 methods)
+```
+
+Cuando `gt00` es un `VarCPIBase` para la base 2000 y su vector de exclusión es
+el primero de la lista: 
+```julia-repl 
+julia> InfExc(gt00, 1) 
+120-element Vector{Float32}:
+ 1.4066696
+ 0.7496357
+ 0.5115509
+ 0.39703846
+ ⋮
+ 0.23813248
+ 0.27743578
+ 0.46629906
+```
+
+Cuando `gt10` es un `VarCPIBase` para la base 2010 y su vector de exclusión es
+el segundo de la lista: 
+```julia-repl 
+julia> InfExc(gt10, 2)
+```
+
+Cuando `gtdata` es un `CountryStructure`: 
+```julia-repl 
+julia> InfExc(gtdata)
+231-element Vector{Float32}:
+ 8.727622
+ 8.73872
+ 8.972383
+ 9.031701
+ ⋮
+ 4.8354387
+ 5.24472
+ 6.0075045
+```
+"""
+Base.@kwdef struct InflationFixedExclusionCPI{N} <: InflationFunction
+    # Tupla con vectores de gastos básicos a excluir en cada base (tantos vectores como bases)
+    v_exc::NTuple{N,Vector{Int}}
+end
+
+# Ampliación para que reciba solo los vectores, no necesariamente como una tupla.
+InflationFixedExclusionCPI(v_exc...) = InflationFixedExclusionCPI(v_exc)
+
+# Extender el método de nombre 
+measure_name(::InflationFixedExclusionCPI) = "Exclusión fija de gastos básicos"
+
+# Cómputo del resumen intermensual utilizando la lista de exclusión i
+function (inflfn::InflationFixedExclusionCPI)(base::VarCPIBase{T}, i::Int) where T 
+    # Elección del vector de exclusión a utlizar dependiendo de que base se está tomando 
+    # 1 -> base 2000, 2-> base 2010 (se puede generalizar para más bases)  
+    exc = inflfn.v_exc[i]
+    # Capitalizar los índices de precios a partir del objeto base::VarCPIBase
+    base_ipc= capitalize(base.v, base.baseindex)
+    # creación de una copia de la lista original de pesos desde base.w
+    w_exc = copy(base.w)
+    # Asignación de peso cero a los gastos básicos de la lista de exclusión (exc = inflfn.v_exc[i]) 
+    # (j itera sobre los elementos de la lista de exclusión)
+        for j in exc w_exc[j] = 0 end
+    # Renormalización de pesos
+    w_exc = w_exc / sum(w_exc)
+    # Obtener Ipc con exclusión 
+    cpi_exc = base_ipc*w_exc
+    # Obtener variación intermensual
+    varm_cpi_exc =  varinterm(cpi_exc)
+end
+
+
+# Redefinición del método para obtener variaciones intermensuales del CountryStructure
+function (inflfn::InflationFixedExclusionCPI)(cs::CountryStructure, ::CPIVarInterm) 
+    # Acá se llama a inflfn(base, i), en donde base es de tipo VarCPIBase e i es la posición del vector de exclusión. 
+    # Esta es la función que debe definirse para cualquier medida de inflación.
+    l = length(cs.base)
+    vm = mapfoldl(i -> inflfn(cs.base[i],i), vcat, 1:l)
+    vm
+end
