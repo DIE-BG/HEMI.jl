28937f4a2bead17e37351365ba5ab77cec5c8015
Eva María de León Fajardo <evamariadeleon@gmail.com>
2021-07-09 17:51:53 -0600

    documentación InflationPercentile Eq y Weghted, script de pruebas

--- src/InflationFunctions/src/InflationPercentileEq.jl
+++ src/InflationFunctions/src/InflationPercentileEq.jl
@@ -2,20 +2,92 @@
 
 # InflationPercentileEq.jl - Definición de percentiles equiponderados
 
-## Percentil equiponderado
+"""
+    InflationPercentileEq <: InflationFunction
+Función de inflación para computar el percentil equiponderado `k`.
 
-# k representa el cuantil equiponderado (entre 0 y 1)
+## Utilización
+
+    function (inflfn::InflationPercentileEq)(base::VarCPIBase{T}) where T 
+
+Define cómo opera `InflationPercentileEq` sobre un objeto de tipo VarCPIBase.
+
+    function (inflfn::InflationPercentileEq)(cs::CountryStructure) 
+
+Define cómo opera `InflationPercentileEq` sobre un objeto de tipo CountryStructure.
+
+### Ejemplo
+Cálculo del percentil 70 de la distribución de variaciones intermensuales ponderadas
+
+```julia-repl
+julia> percEqfn = InflationPercentileEq(70)
+(::InflationPercentileEq) (generic function with 5 methods)
+julia>percEqfn(gtdata) #gtdata es de tipo UniformCountryStructure
+231-element Vector{Float32}:
+9.812069
+10.271847
+10.382021
+10.323441
+10.427952 
+⋮
+1.9042492
+1.9850135
+1.9871473
+2.0293117
+2.1856546
+```
+"""
 Base.@kwdef struct InflationPercentileEq <: InflationFunction
     k::Float32
 end
 
 # Métodos para crear funciones de inflación a partir de enteros y flotantes
+"""
+    InflationPercentileEq(k::Int) = InflationPercentileEq(k = Float32(k) / 100)
+    InflationPercentileEq(q::T) where {T <: AbstractFloat} 
+Permite utilizar valores de `k` que no necesariamente son de tipo de punto flotante, como por ejemplo: enteros, fracciones, Float32 y Float64.
+
+# Ejemplo
+Se usa 0.70 en lugar de 70
+
+```julia-repl
+julia> percEqfn = InflationPercentileEq(0.70)
+(::InflationPercentileEq) (generic function with 5 methods)
+
+julia>percEqfn(gtdata) 
+231-element Vector{Float32}:
+9.812069
+10.271847
+10.382021
+10.323441
+10.427952
+⋮
+1.9042492
+1.9850135
+1.9871473
+2.0293117
+2.1856546
+```
+Se obtienen los mismos resultados.
+"""
 InflationPercentileEq(k::Int) = InflationPercentileEq(k = Float32(k) / 100)
 function InflationPercentileEq(q::T) where {T <: AbstractFloat} 
     q < 1.0 && return InflationPercentileEq(convert(Float32, q))
     InflationPercentileEq(convert(Float32, q) / 100)
 end
 
+"""
+    measure_name(inflfn::InflationPercentileEq)
+
+Indica qué medida se utiliza para una instancia de una función de inflación.
+
+# Ejemplo
+```julia-repl
+julia> percEqfn = InflationPercentileEq(0.70)
+julia> measure_name(percEqfn)
+"Percentil equiponderado 70.0"
+```
+"""
 measure_name(inflfn::InflationPercentileEq) = "Percentil equiponderado " * string(round(100inflfn.k, digits=2))
 
 # Las funciones sobre VarCPIBase resumen en variaciones intermensuales
--- src/InflationFunctions/src/InflationPercentileWeighted.jl
+++ src/InflationFunctions/src/InflationPercentileWeighted.jl
@@ -2,19 +2,37 @@
 
 # InflationPercentileWeighted.jl - Definición de percentiles ponderados
 
-
-## Percentil ponderado
-
-# k representa el cuantil ponderado (entre 0 y 1)
 """
     InflationPercentileWeighted <: InflationFunction
-
 Función de inflación para computar el percentil ponderado k.
 
-## Ejemplos 
+## Utilización
+
+    function (inflfn::InflationPercentileWeighted)(base::VarCPIBase{T}) where T 
+
+Define cómo opera `InflationPercentileWeighted` sobre un objeto de tipo VarCPIBase.
+
+    function (inflfn::InflationPercentileWeighted)(cs::CountryStructure) 
+
+### Ejemplo
+Cálculo del percentil 70 de la distribución de variaciones intermensuales ponderadas
 
 ```julia-repl
 julia> percfn = InflationPercentileWeighted(70)
+(::InflationPercentileWeighted) (generic function with 5 methods)
+julia>percfn(gtdata) #gtdata es de tipo UniformCountryStructure
+231-element Vector{Float32}:
+ 11.189365
+ 11.571873
+ 11.738467
+ 11.552155
+ 11.763763
+  ⋮
+  1.8916845
+  2.074194
+  2.0474315
+  2.1219969
+  2.2268414
 ```
 """
 Base.@kwdef struct InflationPercentileWeighted <: InflationFunction
@@ -22,15 +40,55 @@ Base.@kwdef struct InflationPercentileWeighted <: InflationFunction
 end
 
 # Métodos para crear funciones de inflación a partir de enteros y flotantes
+"""
+    InflationPercentileWeighted(k::Int) = InflationPercentileWeighted(k = Float32(k) / 100)
+    InflationPercentileWeighted(q::T) where {T <: AbstractFloat} 
+Permite utilizar valores de `k` que no necesariamente son de tipo de punto flotante, como por ejemplo: enteros, fracciones, Float32 y Float64.
+
+# Ejemplo
+Se usa 0.70 en lugar de 70
+
+```julia-repl
+julia> percfn = InflationPercentileWeighted(0.70)
+(::InflationPercentileWeighted) (generic function with 5 methods)
+
+julia>percfn(gtdata) 
+231-element Vector{Float32}:
+ 11.189365
+ 11.571873
+ 11.738467
+ 11.552155
+ 11.763763
+  ⋮
+  1.8916845
+  2.074194
+  2.0474315
+  2.1219969
+  2.2268414
+```
+Se obtienen los mismos resultados
+"""
 InflationPercentileWeighted(k::Int) = InflationPercentileWeighted(k = Float32(k) / 100)
 function InflationPercentileWeighted(q::T) where {T <: AbstractFloat} 
     q < 1.0 && return InflationPercentileWeighted(convert(Float32, q))
     InflationPercentileWeighted(convert(Float32, q) / 100)
 end
 
+"""
+    measure_name(inflfn::InflationPercentileWeighted)
+
+Indica qué medida se utiliza para una instancia de una función de inflación.
+
+# Ejemplo
+```julia-repl
+julia> percfn = InflationPercentileWeighted(0.70)
+julia> measure_name(percfn)
+"Percentil ponderado 70.0"
+```
+"""
 measure_name(inflfn::InflationPercentileWeighted) = "Percentil ponderado " * string(round(100inflfn.k, digits=2))
 
-# Las funciones sobre VarCPIBase resumen en variaciones intermensuales
+# Las funciones sobre VarCPIBase se resumen en variaciones intermensuales
 function (inflfn::InflationPercentileWeighted)(base::VarCPIBase{T}) where T 
     # InflationPercentileWeighted k de la distribución de variaciones intermensuales
     k = inflfn.k
