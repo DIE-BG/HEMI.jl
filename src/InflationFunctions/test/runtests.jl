9ba05e97387e1839b8f5a89c7bc80ebb11e3d748
Eva María de León Fajardo <evamariadeleon@gmail.com>
2021-07-12 12:43:42 -0600

    traducción CPIDataBase; adición de test de percentiles en InflationFunctions

--- src/InflationFunctions/test/runtests.jl
+++ src/InflationFunctions/test/runtests.jl
@@ -39,3 +39,69 @@ end
     # Probamos que la trayectoria de inflación sea igual a cero 
     @test all(traj_infl .≈ 0)
 end
+
+# Función de inflación por percentiles equiponderados
+# prueba con percentil 70
+@testset "InflationPercentileEq" begin
+    # Instanciar un tipo 
+    percEqfn = InflationPercentileEq(70) 
+    @test percEqfn isa InflationPercentileEq
+
+    # Probar que esté definido el método para obtener su nombre 
+    @test measure_name(percEqfn) isa String
+    @test measure_tag(percEqfn) isa String
+
+    # Probar con bases del IPC con variaciones intermensuales iguales a cero.
+    # Estas pruebas ayudan a verificar que la función de inflación se pueda
+    # llamar sobre los tipos correctos 
+   
+    zero_base = getzerobase()
+    m_traj_infl = percEqfn(zero_base)
+    # Probamos que el resumen intermensual sea igual a cero
+    @test all(m_traj_infl .≈ 0)
+
+    # Obtenemos un UniformCountryStructure con dos bases y todas las variaciones
+    # intermensuales iguales a cero
+    zero_cst = getzerocountryst()
+    traj_infl = percEqfn(zero_cst)
+    
+    # Probamos que la trayectoria de inflación sea más larga que el resumen
+    # intermensual de una sola base 
+    @test length(traj_infl) > length(m_traj_infl)
+
+    # Probamos que la trayectoria de inflación sea igual a cero 
+    @test all(traj_infl .≈ 0)
+end
+
+# Función de inflación por percentiles ponderados
+# prueba con percentil 70
+@testset "InflationPercentileWeighted" begin
+    # Instanciar un tipo 
+    percfn = InflationPercentileWeighted(70) 
+    @test percfn isa InflationPercentileWeighted
+
+    # Probar que esté definido el método para obtener su nombre 
+    @test measure_name(percfn) isa String
+    @test measure_tag(percfn) isa String
+
+    # Probar con bases del IPC con variaciones intermensuales iguales a cero.
+    # Estas pruebas ayudan a verificar que la función de inflación se pueda
+    # llamar sobre los tipos correctos 
+   
+    zero_base = getzerobase()
+    m_traj_infl = percfn(zero_base)
+    # Probamos que el resumen intermensual sea igual a cero
+    @test all(m_traj_infl .≈ 0)
+
+    # Obtenemos un UniformCountryStructure con dos bases y todas las variaciones
+    # intermensuales iguales a cero
+    zero_cst = getzerocountryst()
+    traj_infl = percfn(zero_cst)
+    
+    # Probamos que la trayectoria de inflación sea más larga que el resumen
+    # intermensual de una sola base 
+    @test length(traj_infl) > length(m_traj_infl)
+
+    # Probamos que la trayectoria de inflación sea igual a cero 
+    @test all(traj_infl .≈ 0)
+end
