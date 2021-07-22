c8455f24e519e755074dcef2e7d960fedecb58b6
Marianojgm86 <59839138+Marianojgm86@users.noreply.github.com>
2021-07-22 11:39:06 -0600

    Scrip evaluación medidas base
    Creación de script para evaluación de medidas base:
    - DAMP alimentos y energéticos
    - DAMP energéticos
    - DIE Todo alimentos y energéticos

--- scripts/fixed-exclusion/eval-FixedExclusionCPI.jl
+++ scripts/fixed-exclusion/eval-FixedExclusionCPI.jl
@@ -13,9 +13,10 @@ addprocs(4, exeflags="--project")
 """
 ref: https://github.com/DIE-BG/EMI/blob/master/%2BEMI/%2Bexclusion_fija/exclusion_alternativas.m
 1. Evaluación de medidas de exclusión fija 
- - Alimentos y energéticos variante 1
- - Energéticos 
- - Alimentos y energéticos variante 2
+ - DAMP alimentos y energéticos
+ - DAMP energéticos 
+ - DIE Todo alimentos y energéticos
+ - DIE Exclusión óptima por volatilidad
 
 """
 ## Elementos generales evaluación
@@ -23,27 +24,19 @@ gtdata_eval = gtdata[Date(2020, 12)]
 resamplefn = ResampleSBB(36)
 trendfn = TrendRandomWalk()
 
-# Inflación Inflation total
-tot = InflationTotalCPI()
-Infl_total = tot(gtdata)
 ## Vectores de exclusión por medida
-# 1. Alimentos y energéticos variante 1 (11)
+# 1. DAMP alimentos y energéticos
 
-exc_ae1 = (vcat(collect(23:41), 104, 159), vcat(collect(22:48), 116, collect(184:186)))
+exc_damp1 = (vcat(collect(23:41), 104, 159), vcat(collect(22:48), 116, collect(184:186)))
 
-# 2.Energéticos (11)
-exc_e  = ([104, 159], vcat(116, collect(184:186)))
+# 2. DAMP energéticos
+exc_damp2 = ([104, 159], vcat(116, collect(184:186)))
 
-# 3. Todo alimentos y energéticos (9)
-exc_ae2 = (vcat(collect(1:62), 104, 159), vcat(collect(1:74), collect(116:118), collect(184:186)))
-
-# 4. Excluión fija óptima (9)
-opt00 = [35, 30, 190, 36, 37, 40, 31, 104, 162, 32, 33, 159, 193, 161, 50, 160, 21, 163, 3, 4, 97, 2, 27, 1, 191, 188]
-opt10 = [29, 46, 39, 31, 116]
-exc_opt = (opt00, opt10)
+# 3. DIE todo alimentos y energéticos
+exc_die1 = (vcat(collect(1:62), 104, 159), vcat(collect(1:74), collect(116:118), collect(184:186)))
 
 ## Creación de diccionario para simulación y savepath
-list = [exc_ae1, exc_e, exc_ae2, exc_opt]
+list = [exc_damp1, exc_damp2, exc_die1]
 
 sim_FxEx = Dict(
     :inflfn => InflationFixedExclusionCPI.(list), 
@@ -52,7 +45,7 @@ sim_FxEx = Dict(
     :nsim => 125000) |> dict_list
 
 
-savepath = datadir("results","fixed-exclusion","Medidas-base-125K")
+savepath = datadir("fixed-exclusion","Medidas-base")
 
 ## lote de simulación 
 
@@ -61,40 +54,3 @@ run_batch(gtdata_eval, sim_FxEx, savepath)
 ## resultados
 
 FxEx_base = collect_results(savepath)
-
-## Trayectorias hasta Febrero 2021
-AE_v1 = InflationFixedExclusionCPI(exc_ae1)(gtdata)
-Energ = InflationFixedExclusionCPI(exc_e)(gtdata)
-AE_v2 = InflationFixedExclusionCPI(exc_ae2)(gtdata)
-Opt = InflationFixedExclusionCPI(exc_opt)(gtdata)
-param = InflationTotalRebaseCPI() 
-param_tray_infl = param(gtdata)
-
-
-saveplot = plotsdir("fixed-exclusion", "Medidas-Base")
-plotrng = Date(2001, 12):Month(1):Date(2021, 6)
-using Plots
-tray_plot = plot(plotrng, Opt, label = "Exclusión Fija Óptima",
-title = "Medidas de Exclusión Fija", dpi=150) 
-plot!(plotrng, AE_v1, label= "Alimentos y Energéticos variante 1")
-plot!(plotrng, Energ, label = "Energéticos")
-plot!(plotrng, AE_v2, label = "Alimentos y Energéticos variante 2")  
-plot!(plotrng, Infl_total, label = "Inflación Total")
-plot!(plotrng, param_tray_infl, label="Inflación Parámetro")
-
-hspan!([3,5], color=[:gray], alpha=0.25, label="")
-hline!([4], linestyle=:dash, color=[:black], label = "")
-    
-savefig(tray_plot,saveplot)
-
-
-##
-sorteddf = sort(FxEx_base, :mse)
-
-gr = [sorteddf[1,:mse],sorteddf[2,:mse], sorteddf[3,:mse],sorteddf[4,:mse]]
-gr_l = ["Exc. Óptima","Alim y En. 9","Alim. y En. 11","Energéticos 11"]
-graf = plot(gr, seriestype=:bar, xticks = (1:4, gr_l), label="MSE", ylims=[0, 10], dpi=150)
-title!("Comparativo entre Medidas de Exclusión Fija")
-
-saveplot = plotsdir("fixed-exclusion", "Comparativo-ExFija")
-savefig(graf,saveplot)
