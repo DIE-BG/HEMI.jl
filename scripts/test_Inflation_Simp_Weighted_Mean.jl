ee6154a78eafca4eee8392a9a1f2569e6f5981a7
guiselaha <guiselaha@gmail.com>
2021-07-23 15:38:12 -0600

    Inflation Moving Average

--- scripts/test_Inflation_Simp_Weighted_Mean.jl
+++ scripts/test_Inflation_Simp_Weighted_Mean.jl
@@ -16,3 +16,20 @@ weightedmeanfn = InflationWeightedMean()
 a=weightedmeanfn(gt10)
 t = weightedmeanfn(gtdata)
 
+# ##MEDIA MÓVIL
+inflfn = InflationMovingAverage(InflationTotalCPI(), 3)
+
+inflfn(gtdata)
+
+using Plots
+
+all_ma = [InflationMovingAverage(InflationTotalCPI(), i)(gtdata) for i in 1:12] |> 
+    x -> hcat(x...)
+
+plot(infl_dates(gtdata), all_ma,
+    # xlims=(Date(2001,12), Date(2005,12)),
+    legend=false)
+plot!(infl_dates(gtdata), InflationTotalCPI()(gtdata), 
+    linewidth=3, color=:black) 
+
+# @enter inflfn(gtdata)
