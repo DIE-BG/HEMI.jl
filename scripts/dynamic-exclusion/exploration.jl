7428642520bfb5fc6c0266ad320226e2f07d7550
m4wnn <38122573+m4wnn@users.noreply.github.com>
2021-07-28 12:41:04 -0600

    Script para creación de grilla y evaluación.

---
+++ scripts/dynamic-exclusion/exploration.jl
@@ -0,0 +1,66 @@
+# # Script de prueba para tipos que especifican variantes de simulación
+using DrWatson
+using Chain
+@quickactivate "HEMI"
+
+# Cargar el módulo de Distributed para computación paralela
+using Distributed
+# Agregar procesos trabajadores
+addprocs(4, exeflags="--project")
+
+# Cargar los paquetes utilizados en todos los procesos
+@everywhere using HEMI
+
+# CountryStructure con datos hasta diciembre de 2020
+gtdata_eval = gtdata[Date(2020, 12)]
+
+## Definición de Parámetros de simulación
+
+# Funciones de remuestreo y tendencia
+resamplefn = ResampleSBB(36)
+trendfn = TrendRandomWalk()
+
+## Ejemplos de creacion de diccionarios para Evaluación
+# Estos diccionarios contienen n vectores, en donde cada uno contiene una parametrización de simulación, 
+# la cual posteriormente es convertida a un objeto AbstractConfig y utilizada para generar una simulación.
+
+# Diccionario de prueba 1: utilizando la función dict_list (DrWatson), 
+# crea un vector de tuplas con el producto cartesiano de ...
+
+dict_config_dynEx = Dict(
+    :inflfn => InflationDynamicExclusion.(
+            @chain range(0, 3, length = 70) ((i, j) for i in _ for j in _) 
+       ),
+    #:inflfn => InflationDynamicExclusion(2,2),
+    :resamplefn => resamplefn, 
+    :trendfn => trendfn,
+    :nsim => 10_000) |> dict_list
+
+
+## FUNCIONES DE EVALUACIÓN
+
+# run_batch 
+
+# Esta función recibe un CountryStructure, un vector con diccionarios con
+# parámetros de evaluación y una ruta para almacenar los resultados.
+
+# Path para almacenamiento de resultados.
+savepath = datadir("results", "dynamic-exclusion")
+
+run_batch(gtdata_eval, dict_config_dynEx, savepath)
+
+# Revisión de resultados
+# La función collect_results recoje todos los resultados almacenados en savepath
+# y los almacena en un DataFrame
+
+df = collect_results(savepath)
+df = df[.!isnan.(df.rmse), :]
+
+df_min = df[minimum(df.rmse) .=== df.rmse, :]
+# Revisamos el elemento del DataFrame con el valor mínimo de mse
+minimum(df[!,"mse"])
+# (0.42424244f0, 1.5151515f0)
+
+# Podemos ordenar el DataFrame respecto al mse.
+sorted_df = sort(df, "mse")
+
