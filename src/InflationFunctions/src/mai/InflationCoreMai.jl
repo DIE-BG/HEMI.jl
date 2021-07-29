abb25b460a854fb0be7db54a745f237daab4297d
Rodrigo Chang <rrcp777@gmail.com>
2021-07-29 12:18:51 -0600

    División de métodos versión F y G

--- src/InflationFunctions/src/mai/InflationCoreMai.jl
+++ src/InflationFunctions/src/mai/InflationCoreMai.jl
@@ -40,41 +40,59 @@ measure_tag(inflfn::InflationCoreMai) = string(nameof(inflfn)) * string(inflfn.m
 
 # Operación sobre CountryStructure para obtener variaciones intermensuales de la
 # estructura de país
-function (inflfn::InflationCoreMai{T})(cs::CountryStructure, ::CPIVarInterm) where T
+function (inflfn::InflationCoreMai)(cs::CountryStructure, ::CPIVarInterm)
+    inflfn(cs, CPIVarInterm(), inflfn.method)
+end 
+
+function (inflfn::InflationCoreMai{T})(cs::CountryStructure, ::CPIVarInterm, method::MaiG) where T
+    # Computar flp y glp, tomando en cuenta observaciones de años completos en
+    # la última base del CountryStructure
+    V_star = _get_vstar(cs)
+    W_star = _get_wstar(cs)
+    glp = WeightsDistr(V_star, W_star, inflfn.vspace)
+
+    # Obtener distribuciones acumuladas y sus percentiles 
+    GLP = cumsum(glp)
+    p = (0:method.n) / method.n
+    q_glp::Vector{T} = quantile(GLP, p)
+
+    # Llamar al método de cómputo de inflación intermensual
+    vm_fn = base -> inflfn(base, inflfn.method, glp, GLP, q_glp)
+    vm = mapfoldl(vm_fn, vcat, cs.base)
+    vm
+end
+
+function (inflfn::InflationCoreMai{T})(cs::CountryStructure, ::CPIVarInterm, method::MaiF) where T
 
     # Grilla de variaciones, número de segmentos, cuantiles
-    vspace = inflfn.vspace
-    n::Int = inflfn.method.n
-    p = (0:n) / n
+    p = (0:method.n) / method.n
     
     # Intuitivamente, las distribuciones de largo plazo podrían computarse más
     # sencillamente de esta forma. Sin embargo, parece que hay problemas de
     # precisión en los vectores dispersos al agregar las distribuciones de cada
-    # base de esta manera. Por lo que se computan las distribuciones de largo
-    # plazo con el código de arriba.
+    # base de esta manera.
 
     # flp_bases = ObservationsDistr.(cs.base, Ref(vspace))
     # glp_bases = WeightsDistr.(cs.base, Ref(vspace))
     # flp = sum(flp_bases)
     # glp = sum(glp_bases)
-    
+
     # Computar flp y glp, tomando en cuenta observaciones de años completos en
     # la última base del CountryStructure
     V_star = _get_vstar(cs)
     W_star = _get_wstar(cs)
-    flp = ObservationsDistr(V_star, vspace)
-    glp = WeightsDistr(V_star, W_star, vspace)
+    flp = ObservationsDistr(V_star, inflfn.vspace)
+    glp = WeightsDistr(V_star, W_star, inflfn.vspace)
 
 
     # Obtener distribuciones acumuladas y sus percentiles 
     FLP = cumsum(flp)
     GLP = cumsum(glp)
-
-    q_glp::Vector{T} = quantile(GLP, p)
+    # q_glp::Vector{T} = quantile(GLP, p)
     q_flp::Vector{T} = quantile(FLP, p)
 
     # Llamar al método de cómputo de inflación intermensual
-    vm_fn = base -> inflfn(base, inflfn.method, glp, FLP, GLP, q_glp, q_flp)
+    vm_fn = base -> inflfn(base, inflfn.method, glp, GLP, q_flp)
     vm = mapfoldl(vm_fn, vcat, cs.base)
     vm
 end
@@ -104,9 +122,12 @@ function _get_wstar(cs::CountryStructure)
     W_star
 end
 
+## Métodos de cómputo MAI sobre VarCPIBase,
+# Se utiliza información de largo plazo provista por el método que opera sobre
+# CountryStructure y CPIVarInterm
 
 # Variaciones intermensuales resumen con método de MAI-G
-function (inflfn::InflationCoreMai)(base::VarCPIBase{T}, method::MaiG, glp, FLP, GLP, q_glp, q_flp) where T
+function (inflfn::InflationCoreMai)(base::VarCPIBase{T}, method::MaiG, glp, GLP, q_glp) where T
 
     mai_m = Vector{T}(undef, periods(base))
     q_g_list = [zeros(T, method.n+1) for _ in 1:Threads.nthreads()]
@@ -136,7 +157,7 @@ function (inflfn::InflationCoreMai)(base::VarCPIBase{T}, method::MaiG, glp, FLP,
 end
 
 # Variaciones intermensuales resumen con método de MAI-F
-function (inflfn::InflationCoreMai)(base::VarCPIBase{T}, method::MaiF, glp, FLP, GLP, q_glp, q_flp) where T
+function (inflfn::InflationCoreMai)(base::VarCPIBase{T}, method::MaiF, glp, GLP, q_flp) where T
 
     mai_m = Vector{T}(undef, periods(base))
     q_f_list = [zeros(T, method.n+1) for _ in 1:Threads.nthreads()]
