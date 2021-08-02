5b39b72497df7e7a9dc040f58c7de1bc2072b284
edmorca <85895115+edmorca@users.noreply.github.com>
2021-08-02 12:24:42 -0600

    Changes to InflationExpSmoothing
    Ultimos cambios en referencia al feedback recibido.

--- src/InflationFunctions/src/InflationExpSmoothing.jl
+++ src/InflationFunctions/src/InflationExpSmoothing.jl
@@ -1,17 +1,18 @@
-# ExpSmoothing.jl - Tipo para computar medias móviles 
-# con suavizamiento exponencial de medidas de inflación
+# ExpSmoothing.jl - Tipo para computar medias con 
+# suavizamiento exponencial de medidas de inflación
 
 """
-    InflationMovingAverage{F <: InflationFunction} <: InflationFunction
-Función de inflación que computa el promedio móvil de `k` períodos de la
-trayectoria interanual de la medida de inflación `inflfn` almacenada. 
+    InflationExpSmoothing{F <: InflationFunction} <: InflationFunction
+Función de inflación que computa la inflación con decaimiento exponencial con  con el parámetro
+alpha (el parámetro del decaimiento ) de la trayectoria interanual de la medida de inflación
+`inflfn` almacenada. 
 
 ## Ejemplo
 
-Para computar la media móvil de 6 meses de la variación interanual del IPC: 
+Para computar la inflación con decaimiento exponencial de la variación interanual del IPC: 
 ```julia-repl 
-julia> inflfn = InflationMovingAverage(InflationTotalCPI(), 6)
-(::InflationMovingAverage{InflationTotalCPI}) (generic function with 5 methods)
+julia> inflfn = InflationExpSmoothing(InflationTotalCPI(), 0.8)
+(::InflationExpSmoothing{InflationTotalCPI}) (generic function with 5 methods)
 ```
 """
 struct InflationExpSmoothing{F <: InflationFunction} <: InflationFunction
@@ -20,13 +21,15 @@ struct InflationExpSmoothing{F <: InflationFunction} <: InflationFunction
 end
 
 # Método que opera sobre CountryStructure: computa la trayectoria de inflación
-# con la función inflfn y luego computa el promedio móvil de k períodos
+# con la función inflfn y luego computa el promedio con suavizamiento exponencial con el parámetro
+# alpha (el parámetro del decaimiento )
+
 function (esfn::InflationExpSmoothing)(cs::CountryStructure)
 
     # Cómputo usual de inflación
     tray_infl = esfn.inflfn(cs)
 
-    # Algoritmo de promedio móvil 
+    # Algoritmo de promedio con suavizamiento exponencial
     k = esfn.alpha
     k == 1 && return tray_infl
 
@@ -39,26 +42,25 @@ end
 
 # Actualmente, se define el resumen intermensual por el obtenido con la función
 # de inflación interna
-# function (mafn::InflationMovingAverage)(base::VarCPIBase)
-#     # Obtener resumen intermensual y aplicarle media móvil
+# function (mafn::InflationExpSmoothing)(base::VarCPIBase)
+#     # Obtener resumen intermensual y aplicarle el suavizamiento exponencial
 #     vm = mafn.inflfn(base)
-#     moving_average!(vm, mafn.periods)
 #     vm
 # end
 
-# Función para computar el promedio con suavizamiento exponencial de la serie `v` con `k` como valor de suavizamiento 
-# que pondera la historia 0<=k<=1.  
-# Esta función modifica los elementos de `v` con el promedio de suavizmiento exponencial con lambda dada. 
-# El tipo de suavizamiento que se utiliza es SES (Simple Exponential Smoothing, o Suavizamiento Exponencial Simple en español)
+# Función para computar el promedio con suavizamiento exponencial de la serie `v` con `k` 
+# como valor de suavizamiento que pondera la historia 0<=k<=1.  
+# Esta función modifica los elementos de `v` con el promedio de suavizmiento exponencial 
+# con alpha dada. 
+# El tipo de suavizamiento que se utiliza es SES (Simple Exponential Smoothing, o 
+# Suavizamiento Exponencial Simple en español)
 
 function smoothing_exponential(v, k)
     es = similar(v)
-    es = reverse(es)
-    es[1] = v[1]
     n = length(es)
-    for j = 2:(n-1)
-        a = j-1
-        es[j] = es[j-1] + (v[j] - es[a])*k
+    es[1] = v[n]
+    for j = 2:n
+        es[j] = es[j-1] + (v[n-j+1] - es[(j-1)])*k
     end
     es
 end 
