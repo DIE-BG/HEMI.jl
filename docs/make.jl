af5fa43588ad9a4347cfae373897550561d30a76
m4wnn <38122573+m4wnn@users.noreply.github.com>
2021-08-06 16:28:38 -0600

    Documentación de escenario A para inflación de exclusión dinámica

--- docs/make.jl
+++ docs/make.jl
@@ -38,6 +38,11 @@ makedocs(;
         "Guía rápida" => "guides/Guia-rapida.md",
         "Ejemplos" => example_pages,
         "Guía de evaluación" => "guides/Guia-evaluacion.md", 
+        "Evaluación" => [
+            "Escenario A" => [
+                "eval/EscA/evaluacion-dynEx.md"
+            ]
+        ], 
         "API" => 
             ["modules/API.md",
             "modules/HEMI.md",
