666b366c60a8a693e01cc765da7c1aa09b2d3fb2
m4wnn <38122573+m4wnn@users.noreply.github.com>
2021-08-05 18:29:00 -0600

    Script para la optimización de variantes

--- scripts/dynamic-exclusion/optimization.jl
+++ scripts/dynamic-exclusion/optimization.jl
@@ -1,69 +1,92 @@
-using Optim: lower_bound
-# # Script de prueba para tipos que especifican variantes de simulación
+# # Optimización
+
+using Optim
 using DrWatson
+
 @quickactivate "HEMI"
 
-## Cargar el módulo de Distributed para computación paralela
+# Cargar el módulo de Distributed para computación paralela
 using Distributed
+
 # Agregar procesos trabajadores
 addprocs(4, exeflags="--project")
 
 # Cargar los paquetes utilizados en todos los procesos
 @everywhere using HEMI
 
-## Obtener datos para evaluación 
-# CountryStructure con datos hasta diciembre de 2020
-gtdata_eval = gtdata[Date(2020, 12)]
+# ## Parametros globales
 
-# Funciones de remuestreo y tendencia a utilizar para evaluación 
-resamplefn = ResampleSBB(36)
-trendfn = TrendRandomWalk()
+DATA = gtdata
 
+LOWER_B = [0f0, 0f0]
+UPPER_B = [3f0, 3f0]
 
-## Función de evaluación para optimizador 
-function evalDynEx(factors_vec, evaldata;
-    resamplefn = ResampleSBB(36), trendfn = TrendRandomWalk(), K = 125_000
-)
-    # Crear configuración de evaluación
-    evalconfig = SimConfig(
-        inflfn = InflationDynamicExclusion(factors_vec),
-        resamplefn = resamplefn, 
-        trendfn = trendfn, 
-        nsim = K)
+PARAM_0 = [0.42424244f0, 1.5151515f0]
 
-    # Evaluar la medida y obtener el MSE
-    results, _ = makesim(evaldata, evalconfig)
-    mse = results[:mse]
-    mse
+F_TOL = 1e-6
+
+SAVEPATH = datadir("results", "dynamic-exclusion", "optimization")
+
+# ## Variantes de optimización
+
+optim_variants = Dict(
+    :inflfn => vecParameters -> InflationDynamicExclusion(vecParameters),
+    :resamplefn => [ResampleScrambleVarMonths(), ResampleSBB(36)], 
+    :trendfn => TrendRandomWalk(),
+    :paramfn => [InflationTotalRebaseCPI(36, 2), InflationWeightedMean()],
+    :nsim => 100,
+    :traindate => [Date(2019, 12), Date(2020, 12)]
+) |> dict_list
+
+# ## Optimización de variantes
+for variant in optim_variants
+
+    function mse_variant(vecParameters)
+
+        # Crea configuración de evaluación
+        sim_config = SimConfig(
+            inflfn = variant[:inflfn](vecParameters),
+            resamplefn = variant[:resamplefn], 
+            trendfn = variant[:trendfn], 
+            paramfn = variant[:paramfn],
+            nsim = variant[:nsim],
+            traindate = variant[:traindate]
+        )
+
+        # Evalua la medida y obtiene el MSE
+        if all(LOWER_B .< vecParameters .< UPPER_B)
+            results, _ = makesim(DATA, sim_config)
+            mse = results[:mse]
+        else
+            mse = 1_000_000
+        end
+        
+        return mse
+    end
+
+    # Optimización
+    optres = optimize(
+        mse_variant, # Función
+        LOWER_B, UPPER_B, # Límites
+        PARAM_0, # Punto inicial
+        NelderMead(), # Método
+        Optim.Options(
+            f_tol = F_TOL
+        )
+    )
+
+    println(optres)
+    @info "Resultados de optimización:" min_mse=minimum(optres) minimizer=Optim.minimizer(optres)  iterations=Optim.iterations(optres)
+
+    # Guardar los resultados 
+    variant[:inflfn] = variant[:inflfn](Optim.minimizer(optres))
+    filename = savename(dict_config(variant), "jld2")
+            
+    # Resultados de evaluación para collect_results 
+    save(joinpath(SAVEPATH, filename), Dict("optres" => optres))
+
 end
 
-# Prueba de la función de evaluación 
-evalDynEx([2, 2], gtdata_eval)
 
 
-## Algoritmo de optimización iterativo 
 
-using Optim
-
-lower_b = [0f0, 0f0]
-upper_b = [3f0, 3f0]
-
-initial_params = [0.42424244f0, 1.5151515f0] # mse = 2.9163475f0
-
-
-f = factors_vec -> evalDynEx(factors_vec, gtdata_eval)
-
-
-optres = optimize(f, lower_b, upper_b, initial_params, NelderMead())
-println(optres)
-@info "Resultados de optimización:" min_mse=minimum(optres) minimizer=Optim.minimizer(optres)  iterations=Optim.iterations(optres)
-
-#min_mse = 2.8991117f0
-#[0.43126786f0, 1.4843221f0]
-
-savepath = datadir("results", "dynamic-exclusion", "optimization")
-
-save(
-    datadir(savepath, "optres_dynEx.jld2"),
-    Dict("optres" => optres)
-)
