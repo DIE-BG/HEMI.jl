bb05112144afdf430c417bd08fa9c87180085a87
Rodrigo Chang <rrcp777@gmail.com>
2021-08-09 17:57:30 -0600

    Optimización iterativa Optim

--- scripts/mai/eval-combineCoreMai.jl
+++ scripts/mai/eval-combineCoreMai.jl
@@ -4,12 +4,14 @@ using DrWatson
 
 using HEMI 
 using DataFrames, Chain
+using Optim
 using Plots
 
 # Funciones de ayuda 
 includet(scriptsdir("mai", "eval-helpers.jl"))
 
 # Obtenemos el directorio de trayectorias resultados 
+savepath = datadir("results", "CoreMai", "Esc-A")
 tray_dir = datadir("results", "CoreMai", "Esc-A", "tray_infl")
 plotspath = mkpath(plotsdir("CoreMai"))
 
@@ -24,10 +26,11 @@ df_mai = collect_results(savepath)
 # se combinan todas las versiones F y G
 tray_paths = @chain df_mai begin 
     filter(:measure => s -> !occursin("FP",s), _)
-    select(:measure, :mse, :path => ByRow(p -> joinpath(tray_dir, basename(p))) => :tray_path)
+    select(:measure, :mse, :inflfn, :path => ByRow(p -> joinpath(tray_dir, basename(p))) => :tray_path)
+    sort(:mse)
 end
 
-# Obtener las trayectorias 
+# Obtener las trayectorias de los archivos guardados en el directorio tray_infl 
 tray_list_mai = map(tray_paths.tray_path) do path
     tray_infl = load(path, "tray_infl")
 end
@@ -50,17 +53,12 @@ tray_infl_pob = param(gtdata_eval)
 # Obtener los ponderadores de combinación óptimos para el cubo de trayectorias
 # de inflación MAI 
 a_optim = combination_weights(tray_infl_mai, tray_infl_pob)
+# A, b = combination_weights(tray_infl_mai, tray_infl_pob)
 
 dfweights = DataFrame(
     measure = tray_paths.measure, 
     weight = a_optim
 )
-
-# Gráfica de ponderadores 
-bar(a_df.measure, a_df.weight, 
-    label="Ponderadores óptimos MSE", 
-    xrotation=45)
-savefig(plotsdir(plotspath, "opt_weights_mai"))
 
 ##
 # ## Evaluación de combinación lineal óptima 
@@ -71,3 +69,72 @@ tray_infl_maiopt = sum(tray_infl_mai .* a_optim', dims=2)
 
 metrics = eval_metrics(tray_infl_maiopt, tray_infl_pob)
 @info "Métricas de evaluación:" metrics...
+
+
+## Optimización iterativa con Optim 
+
+msefnwdata!(F, G, a) = msefn!(F, G, a, tray_infl_mai, tray_infl_pob)
+optres = Optim.optimize(
+    Optim.only_fg!(msefnwdata!), # Función objetivo = MSE
+    rand(Float32, 10), # Punto inicial de búsqueda 
+    Optim.LBFGS(), # Algoritmo 
+    Optim.Options(show_trace = true)) 
+
+println(optres)
+println(Optim.minimizer(optres))
+@info "Resultados de optimización:" min_mse=minimum(optres) iterations=Optim.iterations(optres)
+
+# Conformar un DataFrame de ponderadores 
+dfweights = DataFrame(
+    measure = tray_paths.measure, 
+    weight = Optim.minimizer(optres)
+)
+
+
+
+## Prueba con funciones sin utilización de inplace 
+#=
+function mseonly(w, tray_infl, tray_infl_pob) 
+    # Trayectoria promedio ponderado entre las medidas a combinar
+    tray_infl_comb = sum(tray_infl .* w', dims=2)
+
+    # Definición del error como función de los ponderadores
+    err_t_k =  tray_infl_comb .- tray_infl_pob
+
+    # Función objetivo
+    # Definición del MSE promedio en función de los ponderadores
+    mse_prom = mean(err_t_k .^ 2)
+    mse_prom
+end
+
+function gradonly(w, tray_infl, tray_infl_pob) 
+    # Trayectoria promedio ponderado entre las medidas a combinar
+    tray_infl_comb = sum(tray_infl .* w', dims=2)
+
+    # Definición del error como función de los ponderadores
+    err_t_k =  tray_infl_comb .- tray_infl_pob
+
+    # Cómputo de gradientes
+    mse_grad = 2 * vec(mean(err_t_k .* tray_infl, dims=[1, 3]))
+    mse_grad
+end
+
+optres = optimize(
+    a -> mseonly(a, tray_infl_mai, tray_infl_pob), 
+    a -> gradonly(a, tray_infl_mai, tray_infl_pob), 
+    ones(Float32, 10) / 10; 
+    inplace = false, show_trace = true)
+=#
+
+
+## Generación de gráfica de trayectoria histórica 
+
+tray_infl_mai_obs = mapreduce(inflfn -> inflfn(gtdata), hcat, tray_paths.inflfn)
+tray_infl_maiopt = tray_infl_mai_obs * optres.minimizer
+
+plot(InflationTotalCPI(), gtdata)
+plot!(infl_dates(gtdata), tray_infl_maiopt, 
+    label="Combinación lineal óptima MSE MAI", 
+    legend=:topright)
+
+savefig(plotsdir(plotspath, "MAI-optima-MSE.svg"))
--- scripts/mai/eval-helpers.jl
+++ scripts/mai/eval-helpers.jl
@@ -1,5 +1,6 @@
-# Función de combinación de trayectorias 
+## Funciones para obtención de ponderadores de la combinación lineal de trayectorias 
 
+## Método de solución analítica de ponderadores de la combinación lineal óptima del MSE 
 function combination_weights(tray_infl, tray_infl_pob)
     # Número de ponderadores, observaciones y simulaciones 
     T, n, K = size(tray_infl)
@@ -11,13 +12,73 @@ function combination_weights(tray_infl, tray_infl_pob)
         A += (@views tray_infl_T[:, :, j] * tray_infl[:, :, j])
     end
     # Promedios en el tiempo y realizaciones
-    A = A / (T * K)
+    # fullA = convert.(Float64, A)
+    # fullA /= (T * K)
+    @info size(A)
+
+    # fullA = mean(A / T, dims=3)[:, :, 1]
+    fullA = A / (T * K)
+
+    @info size(fullA)
 
     # Interceptos como función del parámetro y las trayectorias a combinar
-    b = vec(mean(tray_infl_pob .* tray_infl, dims=[1, 3]))
+    b = vec(mean(tray_infl .* tray_infl_pob, dims=[1, 3]))
+    # fullb = convert.(Float64, b)
+    fullb = b
 
     # Ponderadores de combinación óptima 
-    a_optim = A\b
+    @info fullA, fullb 
+    a_optim = fullA \ fullb
     a_optim 
-    # A, b
+    # fullA, fullb
+end
+
+
+## Funciones para optimización iterativa optimización para la combinación lineal 
+
+# Esta función computa el MSE y su gradiente, al proveer los ponderadores
+# para combinar las trayectorias tray_infl y evaluar con el parámetro
+# especificado en tray_infl_pob
+function msefn!(F, G, w, tray_infl, tray_infl_pob) 
+    # Trayectoria promedio ponderado entre las medidas a combinar
+    tray_infl_comb = sum(tray_infl .* w', dims=2)
+
+    # Definición del error como función de los ponderadores
+    err_t_k =  tray_infl_comb .- tray_infl_pob
+
+    # Cómputo de gradientes
+    if G !== nothing 
+        mse_grad = 2 * vec(mean(err_t_k .* tray_infl, dims=[1, 3]))
+        for i in eachindex(G)
+            G[i] = mse_grad[i]
+        end
+    end
+    
+    # Función objetivo
+    if F !== nothing 
+        # Definición del MSE promedio en función de los ponderadores
+        mse_prom = mean(err_t_k .^ 2)
+        return mse_prom
+    end
+end
+
+
+# Esta función computa el MSE y el gradiente respecto del vector de
+# ponderaciones w. Es similar a msefn!, pero se guarda para propósitos de
+# depuración
+function msefn(w, tray_infl, tray_infl_pob) 
+    # Trayectoria promedio ponderado entre las medidas a combinar
+    tray_infl_comb = sum(tray_infl .* w', dims=2)
+
+    # Definición del error como función de los ponderadores
+    err_t_k =  tray_infl_comb .- tray_infl_pob
+
+    # Función objetivo
+    # Definición del MSE promedio en función de los ponderadores
+    mse_prom = mean(err_t_k .^ 2)
+    
+    # Cómputo de gradientes
+    mse_grad = 2 * vec(mean(err_t_k .* tray_infl, dims=[1, 3]))
+
+    mse_prom, mse_grad
 end
