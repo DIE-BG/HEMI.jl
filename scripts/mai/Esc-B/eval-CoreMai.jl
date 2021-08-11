c374b37a0d6ce252d0a6da0edd443ea3a01a1929
Rodrigo Chang <rrcp777@gmail.com>
2021-08-11 12:24:26 -0600

    Update eval-CoreMai.jl

--- scripts/mai/Esc-B/eval-CoreMai.jl
+++ scripts/mai/Esc-B/eval-CoreMai.jl
@@ -1,12 +1,16 @@
 # # Script de evaluación de variantes de inflación subyacente MAI
 using DrWatson
 @quickactivate "HEMI"
+using HEMI 
 
-# Cargar el módulo de Distributed para computación paralela
+## Datos de evaluación 
+const EVALDATE = Date(2020,12)
+gtdata_eval = gtdata[EVALDATE]
+
+## Cargar el módulo de Distributed para computación paralela
 using Distributed
 # Agregar procesos trabajadores
 addprocs(4, exeflags="--project")
-
 # Cargar los paquetes utilizados en todos los procesos
 @everywhere using HEMI
 
@@ -23,8 +27,7 @@ maifps = [InflationCoreMai(MaiFP(i)) for i in variants]
 maifs = [InflationCoreMai(MaiF(i)) for i in variants]
 maigs = [InflationCoreMai(MaiG(i)) for i in variants]
 
-# inflfns = vcat(maifs, maigs)
-inflfns = maifps
+inflfns = vcat(maifs, maigs, maifps)
 
 config_mai = Dict(
     :inflfn => inflfns, 
@@ -36,7 +39,7 @@ config_mai = Dict(
 
 
 # Definimos el folder para almacenar los resultados 
-savepath = datadir("results", "CoreMai", "Esc-A")
+savepath = datadir("results", "CoreMai", "Esc-B", "Standard")
 
 ## Ejecutar la simulación 
 # Usamos run_batch para gnenerar la evaluación de las configuraciones en config_mai
@@ -53,7 +56,7 @@ df_mai = collect_results(savepath)
 df_results = @chain df_mai begin 
     select(:measure, :mse, :mse_std_error)
     sort(:mse)
-    filter(:measure => s -> !occursin("FP",s), _)
+    # filter(:measure => s -> !occursin("FP",s), _)
 end
 # select(df_results, :measure => ByRow(s -> match(r"(?:\w), (?:\d{1,2})", s).match |> split))
 vscodedisplay(df_results)
