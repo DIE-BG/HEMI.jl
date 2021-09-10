22443ed36c34e4c8e1031aad7d66767ccd954ec0
Rodrigo Chang <rrcp777@gmail.com>
2021-09-10 13:06:41 -0600

    Lista de funciones

--- scripts/mse-combination-2019/comb_lineal_2019a.jl
+++ scripts/mse-combination-2019/comb_lineal_2019a.jl
@@ -20,9 +20,13 @@ tray_infl_param = legacy_param(gtdata_eval)
 excOpt00 = [35,30,190,36,37,40,31,104,162,32,33,159,193,161]
 excOpt10 = [29,31,116,39,46,40,30,35,186,47,197,41,22,48,185,34,184]
 
-inf_list = [InflationPercentileEq(72), InflationPercentileWeighted(70),
-            InflationTrimmedMeanEq(57.5, 84), InflationTrimmedMeanWeighted(15,97),
-            InflationFixedExclusionCPI(excOpt00, excOpt10), InflationDynamicExclusion(0.3222, 1.7283) 
+inf_list = [
+    InflationPercentileEq(72), 
+    InflationPercentileWeighted(70),
+    InflationTrimmedMeanEq(57.5, 84), 
+    InflationTrimmedMeanWeighted(15,97),
+    InflationFixedExclusionCPI(excOpt00, excOpt10),
+    InflationDynamicExclusion(0.3222, 1.7283) 
 ]
 
 sims = Dict(
