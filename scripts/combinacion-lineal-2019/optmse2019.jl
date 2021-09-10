f74c3093c4829e0181785cffdd82bbe228ac0357
Rodrigo Chang <rrcp777@gmail.com>
2021-09-10 12:55:35 -0600

    Función de inflación para óptima MSE v2019

--- scripts/combinacion-lineal-2019/optmse2019.jl
+++ scripts/combinacion-lineal-2019/optmse2019.jl
@@ -1,18 +1,9 @@
 # Función de combinación lineal óptima MSE 2019
 
-let 
+optmse2019 = let 
     # Listas de exclusión óptima 
     excOpt00 = [35,30,190,36,37,40,31,104,162,32,33,159,193,161]
     excOpt10 = [29,31,116,39,46,40,30,35,186,47,197,41,22,48,185,34,184]
-
-    # Componentes de inflación subyacente 
-    components = [
-        InflationPercentileEq(72), 
-        InflationPercentileWeighted(70),
-        InflationTrimmedMeanEq(57.5, 84), 
-        InflationTrimmedMeanWeighted(15,97),
-        InflationFixedExclusionCPI(excOpt00, excOpt10), InflationDynamicExclusion(0.3222, 1.7283) 
-    ]
 
     # Componentes metodologías MAI 
     maifns = [
@@ -42,17 +33,6 @@ let
         0.054515,
     ]
 
-    # Ponderaciones de las demás componentes 
-    mse_weights = Float32[
-        -0.0895958,
-        0.287025,
-        1.1065,
-        -0.134828,
-        -0.36216 ,
-        0.0141849,
-        0.177114,
-    ]
-
     # Subyacente óptima MAI por método MSE 
     optmai = CombinationFunction(
         maifns..., 
@@ -60,9 +40,31 @@ let
         "MAI óptima MSE"
     )
 
+    # Componentes de inflación subyacente 
+    components = [
+        InflationPercentileEq(72), 
+        InflationPercentileWeighted(70),
+        InflationTrimmedMeanEq(57.5, 84), 
+        InflationTrimmedMeanWeighted(15,97),
+        InflationDynamicExclusion(0.3222, 1.7283), 
+        InflationFixedExclusionCPI(excOpt00, excOpt10), 
+        optmai 
+    ]
+
+    # Ponderaciones de las demás componentes 
+    mse_weights = Float32[
+        -0.36216 ,
+        0.0141849,
+        1.1065,
+        -0.134828,
+        -0.0895958,
+        0.287025,
+        0.177114,
+    ]
+
     # Subyacente óptima MSE v2019
     optmse2019 = CombinationFunction(
-        components..., optmai, 
+        components...,
         mse_weights, 
         "Subyacente óptima MSE 2019"
     )
