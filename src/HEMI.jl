0b1efc013c8f787d24463a5697f4481fdbb2ac30
Rodrigo Chang <rrcp777@gmail.com>
2021-09-13 11:33:09 -0600

    Función load_data en HEMI.jl

--- src/HEMI.jl
+++ src/HEMI.jl
@@ -17,27 +17,32 @@ module HEMI
     @reexport using InflationFunctions
     @reexport using InflationEvalTools
 
-    ## Carga de datos de Guatemala
-    export gt00, gt10, gtdata
+    # Datos de prueba del proyecto 
+    testdatafile = datadir("guatemala", "gtdata32_test.jld2")
+    # Archivo de datos principales 
+    datafile = datadir("guatemala", "gtdata32.jld2")
     
+    ## Estructuras de datos de Guatemala
+    export gt00, gt10, gtdata
+       
     function __init__()
-        # Datos de prueba del proyecto 
-        testdata = datadir("guatemala", "gtdata32_test.jld2")
-        # Archivo de datos principales 
-        datafile = datadir("guatemala", "gtdata32.jld2")
-
-        if !isfile(datafile) 
-            datafile = testdata
-            @warn "Correr el script de actualización de datos `load_data.jl` y ejecutar `HEMI.__init__()`"
+        # Cargar los datos 
+        load_data(datafile, testdatafile)
+        @info "Exportando datos en variables `gt00`, `gt10`, `gtdata`"
+    end
+    
+    function load_data(maindatafile=datafile, testdatafile=testdatafile) 
+        if !isfile(maindatafile) 
+            maindatafile = testdatafile
+            @warn "Correr el script de actualización de datos `load_data.jl` y ejecutar `HEMI.load_data()`"
         end 
-        
+
         @info "Cargando datos de Guatemala..."
-        global gt00, gt10 = load(datafile, "gt00", "gt10")
+        global gt00, gt10 = load(maindatafile, "gt00", "gt10")
         global gtdata = UniformCountryStructure(gt00, gt10)
 
         # Exportar datos del módulo 
-        @info "Archivo de datos cargado" data=datafile gtdata
-            
+        @info "Archivo de datos cargado" data=maindatafile gtdata
     end
-    
+
 end
