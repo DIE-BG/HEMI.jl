ac18a8f086819d9b3b55ce81e7baeb4247a4a0d7
Rodrigo Chang <rrcp777@gmail.com>
2021-09-13 14:58:31 -0600

    Pruebas de media simple y ponderada

--- scripts/test/runtests.jl
+++ scripts/test/runtests.jl
@@ -5,5 +5,8 @@ include("test_HEMI.jl")
 # Prueba de aplicación de tendencia 
 include("test_trend.jl")
 
+# Prueba funciones de media simple, ponderada y medias móviles
+include("test_Inflation_Simp_Weighted_Mean.jl")
+
 # Prueba períodos de evaluación 
 include("test_EvalPeriod.jl")
