a35705c23d0fde1b7a8f9870926031b11f742605
Rodrigo Chang <rrcp777@gmail.com>
2021-09-13 16:08:13 -0600

    Opción para cargar archivo de datos 64 bits

--- src/HEMI.jl
+++ src/HEMI.jl
@@ -20,6 +20,7 @@ module HEMI
     # Datos de prueba del proyecto 
     testdatafile = datadir("guatemala", "gtdata32_test.jld2")
     # Archivo de datos principales 
+    fulldatafile = datadir("guatemala", "gtdata.jld2")
     datafile = datadir("guatemala", "gtdata32.jld2")
     
     ## Estructuras de datos de Guatemala
@@ -27,14 +28,28 @@ module HEMI
        
     function __init__()
         # Cargar los datos 
-        load_data(datafile, testdatafile)
+        load_data()
         @info "Exportando datos en variables `gt00`, `gt10`, `gtdata`"
     end
     
-    function load_data(maindatafile=datafile, testdatafile=testdatafile) 
+    """
+        load_data(; full_precision = false)
+
+    Carga los datos del archivo principal de datos `HEMI.datafile`. Si los datos
+    no están presentes, se cargan datos de prueba del archivo incluido por
+    defecto, `HEMI.testdatafile`.
+    - La opción `full_precision` permite cargar datos con precisión de 64 bits.
+    - Archivo principal: `HEMI.datafile = datadir("guatemala",
+      "gtdata32.jld2")`.
+    - Archivo principal (64 bits): `HEMI.datafile = datadir("guatemala",
+      "gtdata.jld2")`.
+    """
+    function load_data(; full_precision::Bool = false) 
+        global fulldatafile, datafile, testdatafile 
+        maindatafile = full_precision ? fulldatafile : datafile 
         if !isfile(maindatafile) 
             maindatafile = testdatafile
-            @warn "Correr el script de actualización de datos `load_data.jl` y ejecutar `HEMI.load_data()`"
+            @warn "Archivo principal de datos no encontrado. Ejecutar el script de actualización de datos `load_data.jl` y ejecutar `HEMI.load_data(). Cargando datos de prueba...`"
         end 
 
         @info "Cargando datos de Guatemala..."
