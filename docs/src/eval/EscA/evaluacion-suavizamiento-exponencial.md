bcd69cb3bea4a25f3be340daa4f44012f992d9e5
Rodrigo Chang <rrcp777@gmail.com>
2021-09-14 16:14:26 -0600

    Inicio documentación escenario A

---
+++ docs/src/eval/EscA/evaluacion-suavizamiento-exponencial.md
@@ -0,0 +1,27 @@
+# Suavizamiento exponencial 
+
+En esta sección se documentan los resultados del proceso de evaluación de las medidas basadas en el suavizamiento exponencial de la variación interanual del IPC.
+
+## Resultados de evaluación con criterios básicos a diciembre de 2019
+
+| Medida      |    MSE | Error estándar |
+| :---------- | -----: | -------------: |
+Llenar acá...
+
+## Descomposición aditiva del MSE
+
+| Medida                       |    MSE | Comp. Sesgo | Comp. Varianza | Comp. Covarianza |
+| :--------------------------- | -----: | ----------: | -------------: | ---------------: |
+Llenar acá...
+
+## Métricas de evaluación 
+
+
+| Medida                       |   RMSE | Error medio |    MAE |  Huber | Correlación |
+| :--------------------------- | -----: | ----------: | -----: | -----: | ----------: |
+Llenar acá...
+
+## Trayectorias de inflación observada
+
+![Trayectoria observada](images/exponential_smoothing/obs_trajectory.svg)
+
--- scripts/ExpSmoothing/Esc-A.jl
+++ scripts/ExpSmoothing/Esc-A.jl
@@ -38,14 +38,37 @@ run_batch(DATA, dict_config, SAVEPATH)
 
 # ## Revisión de resultados
 df = collect_results(SAVEPATH)
+
+## Gráfica de trayectorias 
 p = plot(InflationTotalCPI(), gtdata, fmt = :svg)
-plot!(InflationExpSmoothing(InflationTotalCPI(), df.params[1][1]), gtdata, fmt = :svg)
+plot!(InflationExpSmoothing(InflationTotalCPI(), df.params[1][1]), gtdata, 
+    label = "Suavizamiento exponencial con parámetro $(df.params[1][1])",
+    fmt = :svg
+)
 
 PLOTSPATH = joinpath("docs", "src", "eval", SETTINGNAME, "images", "exponential_smoothing")
 Plots.svg(p, joinpath(PLOTSPATH, "obs_trajectory"))
 
 
+## Resultados para documentación 
 
+# Tabla de resultados principales del escenario 
+main_results = @chain scenario_results begin 
+    select(:measure, :mse, :mse_std_error)
+end
 
+# Descomposición aditiva del MSE 
+mse_decomp = @chain scenario_results begin 
+    select(:measure, :mse, r"^mse_[bvc]")
+end
+
+# Otras métricas de evaluación 
+sens_metrics = @chain scenario_results begin 
+    select(:measure, :rmse, :me, :mae, :huber, :corr)
+end 
+
+pretty_table(main_results, tf=tf_markdown, formatters=ft_round(4))
+pretty_table(mse_decomp, tf=tf_markdown, formatters=ft_round(4))
+pretty_table(sens_metrics, tf=tf_markdown, formatters=ft_round(4))
 
 
