90c94cb9473af3e7d2a89fb32869b4292046ec2b
Rodrigo Chang <rrcp777@gmail.com>
2021-10-14 15:05:59 -0600

    Actualización de nombre para óptima

--- scripts/mse-combination/optmse2022.jl
+++ scripts/mse-combination/optmse2022.jl
@@ -1,4 +1,4 @@
-# Función de combinación lineal óptima MSE 2020
+# Función de combinación lineal óptima MSE 2022
 
 # Definir la subyacente MAI óptima, calibrada con datos hasta 2018
 optmai2018 = let 
@@ -28,9 +28,9 @@ optfx2018 = InflationFixedExclusionCPI(
     [29, 116, 31, 46, 39, 40, 186, 30, 35, 185, 197, 34, 48, 184]
 )
 
-# Definir la combinación óptima MSE 2020, con componentes optimizadas hasta 2018
+# Definir la combinación óptima MSE 2022, con componentes optimizadas hasta 2018
 # y ponderadores ajustados con datos hasta 2020.
-optmse2020 = let 
+optmse2022 = let 
     # Componentes de inflación subyacente 
     components = [
         InflationPercentileEq(72.3966), 
@@ -53,14 +53,14 @@ optmse2020 = let
         0.2547185
     ]
 
-    # Subyacente óptima MSE v2020
-    optmse2020 = CombinationFunction(
+    # Subyacente óptima MSE v2022
+    optmse2022 = CombinationFunction(
         components...,
         mse_weights, 
-        "Subyacente óptima MSE 2020"
+        "Subyacente óptima MSE 2022"
     )
 
-    optmse2020
+    optmse2022
 end
 
-@info "Definición de funciones óptimas MSE" optmai2018 optfx2018 optmse2020
+@info "Definición de funciones óptimas MSE" optmai2018 optfx2018 optmse2022
--- scripts/updates/updates_optmse.jl
+++ scripts/updates/updates_optmse.jl
@@ -10,24 +10,24 @@ include(scriptsdir("load_data.jl"))
 HEMI.load_data()
 
 # Óptima MSE 2020
-include(scriptsdir("mse-combination", "optmse2020.jl"))
+include(scriptsdir("mse-combination", "optmse2022.jl"))
 
 # Generar la trayectoria observada 
 dates = infl_dates(gtdata) 
 df_optmse = DataFrame(
     dates = dates[1] - Month(11): Month(1) : dates[end],
-    i_optmse = optmse2020(gtdata, CPIIndex()), 
-    m_optmse = optmse2020(gtdata, CPIVarInterm()), 
-    a_optmse = vcat(repeat([missing], 11), optmse2020(gtdata))
+    i_optmse = optmse2022(gtdata, CPIIndex()), 
+    m_optmse = optmse2022(gtdata, CPIVarInterm()), 
+    a_optmse = vcat(repeat([missing], 11), optmse2022(gtdata))
 )
 
 last(df_optmse, 5)
 
 # Tabla de componentes y ponderaciones 
-df_comp = components(optmse2020)
+df_comp = components(optmse2022)
 
 # Obtener componentes interanuales 
-df_components = DataFrame(optmse2020.ensemble(gtdata), df_comp.measure)
+df_components = DataFrame(optmse2022.ensemble(gtdata), df_comp.measure)
 insertcols!(df_components, 1, :dates => dates)
 
 
@@ -35,6 +35,6 @@ insertcols!(df_components, 1, :dates => dates)
 savepath = mkpath(datadir("updates"))
 
 @info "Guardando archivos de resultados"
-CSV.write(joinpath(savepath, "optmse2020.csv"), df_optmse)
-CSV.write(joinpath(savepath, "optmse2020_components.csv"), df_comp)
-CSV.write(joinpath(savepath, "optmse2020_interannual_components.csv"), df_components)
+CSV.write(joinpath(savepath, "optmse2022.csv"), df_optmse)
+CSV.write(joinpath(savepath, "optmse2022_components.csv"), df_comp)
+CSV.write(joinpath(savepath, "optmse2022_interannual_components.csv"), df_components)
