53101678a4e210972995acef3a5de0a53aea1bb3
Rodrigo Chang <rrcp777@gmail.com>
2021-10-21 11:45:55 -0600

    Cambios en archivo de actualización de datos

--- scripts/updates/updates_optmse.jl
+++ scripts/updates/updates_optmse.jl
@@ -9,32 +9,43 @@ using DataFrames, CSV
 include(scriptsdir("load_data.jl"))
 HEMI.load_data()
 
-# Óptima MSE 2020
+## Óptima MSE 2020
 include(scriptsdir("mse-combination", "optmse2022.jl"))
 
 # Generar la trayectoria observada 
 dates = infl_dates(gtdata) 
-df_optmse = DataFrame(
+df_obs_optmse = DataFrame(
     dates = dates[1] - Month(11): Month(1) : dates[end],
     i_optmse = optmse2022(gtdata, CPIIndex()), 
     m_optmse = optmse2022(gtdata, CPIVarInterm()), 
     a_optmse = vcat(repeat([missing], 11), optmse2022(gtdata))
 )
 
-last(df_optmse, 5)
+println(last(df_obs_optmse, 5))
 
 # Tabla de componentes y ponderaciones 
-df_comp = components(optmse2022)
+optmse_components = components(optmse2022)
 
 # Obtener componentes interanuales 
-df_components = DataFrame(optmse2022.ensemble(gtdata), df_comp.measure)
-insertcols!(df_components, 1, :dates => dates)
+optmse_obs_components = DataFrame(optmse2022.ensemble(gtdata), optmse_components.measure)
+insertcols!(optmse_obs_components, 1, :dates => dates)
 
 
-# Guardar los resultados 
+## Componentes MAI óptima 
+
+# DataFrame de componentes
+mai_components = components(optmai2018)
+# Variaciones interanuales
+mai_obs_components = DataFrame(optmai2018.ensemble(gtdata), mai_components.measure)
+insertcols!(mai_obs_components, 1, :dates => dates)
+
+
+## Guardar los resultados 
 savepath = mkpath(datadir("updates"))
 
 @info "Guardando archivos de resultados"
-CSV.write(joinpath(savepath, "optmse2022.csv"), df_optmse)
-CSV.write(joinpath(savepath, "optmse2022_components.csv"), df_comp)
-CSV.write(joinpath(savepath, "optmse2022_interannual_components.csv"), df_components)
+CSV.write(joinpath(savepath, "optmse2022.csv"), df_obs_optmse)
+CSV.write(joinpath(savepath, "optmse2022_components.csv"), optmse_components)
+CSV.write(joinpath(savepath, "optmse2022_interannual_components.csv"), optmse_obs_components)
+CSV.write(joinpath(savepath, "optmse2022_mai_components.csv"), mai_components)
+CSV.write(joinpath(savepath, "optmse2022_mai_interannual_components.csv"), mai_obs_components)
