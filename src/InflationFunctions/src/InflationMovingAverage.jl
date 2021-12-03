e0589f6523b6be0f0f3c68ba44a026376fbf58d0
Rodrigo Chang <rrcp777@gmail.com>
2021-12-03 12:17:43 -0600

    Move measure_tag

--- src/InflationFunctions/src/InflationMovingAverage.jl
+++ src/InflationFunctions/src/InflationMovingAverage.jl
@@ -21,9 +21,6 @@ end
 # 2. Extender el método de nombre 
 measure_name(mafn::InflationMovingAverage) = "Promedios Móvil de $(mafn.periods) períodos de " * measure_name(mafn.inflfn)
 
-# Etiqueta para guardado de archivos
-measure_tag(mafn::InflationMovingAverage) = "MA$(mafn.periods)_" * measure_tag(mafn.inflfn)
-
 # Método que opera sobre CountryStructure: computa la trayectoria de inflación
 # con la función inflfn y luego computa el promedio móvil de k períodos
 function (mafn::InflationMovingAverage)(cs::CountryStructure)
