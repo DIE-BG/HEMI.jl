12db4ff1a4b27eb2b8678211e9e30a26fa8cb490
Rodrigo Chang <rrcp777@gmail.com>
2022-02-17 09:34:39 -0600

    Actualización de script de carga de datos

--- scripts/load_data.jl
+++ scripts/load_data.jl
@@ -4,32 +4,99 @@ using DrWatson
 ## Cargar datos de la base 2000 y 2010 del IPC 
 using Dates, CSV, DataFrames
 using CPIDataBase
-
-# Base 2000
-gt_base00 = CSV.read(datadir("guatemala", "Guatemala_IPC_2000.csv"), 
-    DataFrame, normalizenames=true)
-gt00gb = CSV.read(datadir("guatemala", "Guatemala_GB_2000.csv"), 
-    DataFrame, types=[String, String, Float64])
-
-full_gt00 = FullCPIBase(gt_base00, gt00gb)
-gt00 = VarCPIBase(full_gt00)
-
-# Base 2010
-gt_base10 = CSV.read(datadir("guatemala", "Guatemala_IPC_2010.csv"), 
-    DataFrame, normalizenames=true)
-gt10gb = CSV.read(datadir("guatemala", "Guatemala_GB_2010.csv"), 
-    DataFrame, types=[String, String, Float64])
-
-full_gt10 = FullCPIBase(gt_base10, gt10gb)
-gt10 = VarCPIBase(full_gt10)
-
-## Guardar datos para su carga posterior
 using JLD2
 
-jldsave(datadir("guatemala", "gtdata.jld2"); gt00, gt10)
+## Carga de datos de archivos CSV
+@info "Cargando archivos de datos..."
+# Base 2000
+gt_base00 = CSV.read(datadir("guatemala", "Guatemala_IPC_2000.csv"), DataFrame, normalizenames=true)
+gt00gb = CSV.read(datadir("guatemala", "Guatemala_GB_2000.csv"), DataFrame, types=[String, String, Float64])
+# Base 2010
+gt_base10 = CSV.read(datadir("guatemala", "Guatemala_IPC_2010.csv"), DataFrame, normalizenames=true)
+gt10gb = CSV.read(datadir("guatemala", "Guatemala_GB_2010.csv"), DataFrame, types=[String, String, Float64])
 
-## Conversión a Float32
+@info "Datos cargados exitosamente de archivos CSV"
 
-fgt00 = convert(Float32, gt00)
-fgt10 = convert(Float32, gt10)
-jldsave(datadir("guatemala", "gtdata32.jld2"); gt00=fgt00, gt10=fgt10)
+## Construcción de estructuras de datos
+# Base 2000
+full_gt00_64 = FullCPIBase(gt_base00, gt00gb)
+full_gt00_32 = convert(Float32, full_gt00_64)
+var_gt00_64 = VarCPIBase(full_gt00_64)
+var_gt00_32 = VarCPIBase(full_gt00_32)
+
+# Base 2010
+full_gt10_64 = FullCPIBase(gt_base10, gt10gb)
+full_gt10_32 = convert(Float32, full_gt10_64)
+var_gt10_64 = VarCPIBase(full_gt10_64)
+var_gt10_32 = VarCPIBase(full_gt10_32)
+
+# Estructura contenedora de datos del país
+gtdata_32 = UniformCountryStructure(var_gt00_32, var_gt10_32)
+gtdata_64 = UniformCountryStructure(var_gt00_64, var_gt10_64)
+
+@info "Construcción exitosa de estructuras de datos" gtdata_32 gtdata_64
+
+## Guardar datos para su carga posterior
+
+# Guardar datos en formato JLD2 para su carga posterior 
+@info "Guardando archivos de datos JLD2"
+
+jldsave(datadir("guatemala", "gtdata32.jld2"); 
+    # FullCPIBase    
+    fgt00 = full_gt00_32, 
+    fgt10 = full_gt10_32, 
+    # VarCPIBase
+    gt00 = var_gt00_32, 
+    gt10 = var_gt10_32, 
+    # UniformCountryStructure
+    gtdata = gtdata_32, 
+)
+
+jldsave(datadir("guatemala", "gtdata64.jld2"); 
+    # FullCPIBase    
+    fgt00 = full_gt00_64, 
+    fgt10 = full_gt10_64, 
+    # VarCPIBase
+    gt00 = var_gt00_64, 
+    gt10 = var_gt10_64, 
+    # UniformCountryStructure
+    gtdata = gtdata_64, 
+)
+
+## Construcción de datos de prueba por defecto para el proyecto y la documentación
+@info "Construcción de datos de prueba"
+
+TEST_DATE = Date(2021, 12)
+test_gtdata = gtdata_32[TEST_DATE]
+test_gt00 = test_gtdata[1]
+test_gt10 = test_gtdata[2]
+test_fgt00 = full_gt00_32
+
+f = full_gt10_32.dates .<= TEST_DATE
+test_fgt10 = FullCPIBase(
+    full_gt10_32.ipc[f, :], 
+    full_gt10_32.v[f, :], 
+    full_gt10_32.w, 
+    full_gt10_32.dates[f], 
+    full_gt10_32.baseindex, 
+    full_gt10_32.codes, 
+    full_gt10_32.names
+)
+
+jldsave(datadir("guatemala", "gtdata32_test.jld2"); 
+    # FullCPIBase    
+    fgt00 = test_fgt00, 
+    fgt10 = test_fgt10, 
+    # VarCPIBase
+    gt00 = test_gt00, 
+    gt10 = test_gt10, 
+    # UniformCountryStructure
+    gtdata = test_gtdata, 
+)
+
+# Copiar datos de prueba al directorio de datos de la documentación 
+cp(
+    datadir("guatemala", "gtdata32_test.jld2"), 
+    projectdir("docs", "data", "guatemala", "gtdata32.jld2"),
+    force=true    
+)
--- src/HEMI.jl
+++ src/HEMI.jl
@@ -17,47 +17,54 @@ module HEMI
     @reexport using InflationFunctions
     @reexport using InflationEvalTools
 
-    # Datos de prueba del proyecto 
+    # Datos de prueba del proyecto
     testdatafile = datadir("guatemala", "gtdata32_test.jld2")
+    
     # Archivo de datos principales 
-    fulldatafile = datadir("guatemala", "gtdata.jld2")
-    datafile = datadir("guatemala", "gtdata32.jld2")
+    fulldatafile = datadir("guatemala", "gtdata64.jld2")
+    singledatafile = datadir("guatemala", "gtdata32.jld2")
     
     ## Estructuras de datos de Guatemala
-    export gt00, gt10, gtdata
+    export FGT00, FGT10, GT00, GT10, GTDATA
+    # Deprecated: use the uppercase variables
+    export gt00, gt10, gtdata 
        
     function __init__()
         # Cargar los datos 
+        @info "Exportando datos del IPC en variables `FGT00`, `FGT10`, `GT00`, `GT10`, `GTDATA`"
         load_data()
-        @info "Exportando datos en variables `gt00`, `gt10`, `gtdata`"
     end
     
     """
         load_data(; full_precision = false)
 
-    Carga los datos del archivo principal de datos `HEMI.datafile`. Si los datos
+    Carga los datos del archivo principal de datos `HEMI.singledatafile`. Si los datos
     no están presentes, se cargan datos de prueba del archivo incluido por
     defecto, `HEMI.testdatafile`.
     - La opción `full_precision` permite cargar datos con precisión de 64 bits.
-    - Archivo principal: `HEMI.datafile = datadir("guatemala",
+    - Archivo principal: `HEMI.singledatafile = datadir("guatemala",
       "gtdata32.jld2")`.
-    - Archivo principal (64 bits): `HEMI.datafile = datadir("guatemala",
+    - Archivo principal (64 bits): `HEMI.singledatafile = datadir("guatemala",
       "gtdata.jld2")`.
     """
     function load_data(; full_precision::Bool = false) 
-        global fulldatafile, datafile, testdatafile 
-        maindatafile = full_precision ? fulldatafile : datafile 
+        global fulldatafile, singledatafile, testdatafile 
+        maindatafile = full_precision ? fulldatafile : singledatafile 
         if !isfile(maindatafile) 
             maindatafile = testdatafile
             @warn "Archivo principal de datos no encontrado. Ejecutar el script de actualización de datos `load_data.jl` y ejecutar `HEMI.load_data(). Cargando datos de prueba...`"
         end 
 
         @info "Cargando datos de Guatemala..."
-        global gt00, gt10 = load(maindatafile, "gt00", "gt10")
-        global gtdata = UniformCountryStructure(gt00, gt10)
+        global FGT00, FGT10, GT00, GT10, GTDATA = load(maindatafile, "fgt00", "fgt10", "gt00", "gt10", "gtdata")
+
+        # Deprecated
+        global gt00 = GT00
+        global gt10 = GT10
+        global gtdata = GTDATA
 
         # Exportar datos del módulo 
-        @info "Archivo de datos cargado" data=maindatafile gtdata
+        @info "Archivo de datos cargado" data=maindatafile GTDATA
     end
 
 end
