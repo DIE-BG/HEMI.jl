9ddfde3f8bd91d3ff85e1587bee52e572903f4a4
Rodrigo Chang <rrcp777@gmail.com>
2022-04-12 13:32:01 -0600

    Modificación de márgenes

--- scripts/paper/appendix-sensitivity.jl
+++ scripts/paper/appendix-sensitivity.jl
@@ -23,13 +23,15 @@ fgt10 = FullCPIBase(
 
 # Dates and ticks for the plots
 dates = fgt10.dates
-# date_ticks = first(dates):Month(12):last(dates)
 date_ticks = Date(2010,12):Month(24):last(dates)
-date_str = Dates.format.(date_ticks, dateformat"Y-m")
+date_str = Dates.format.(date_ticks, dateformat"Y")
 
 # Maximum lags for correlation plots
 LAGS = 12
 lags_ticks = (0:2:LAGS, string.(0:2:LAGS))
+
+
+
 ## Negative correlation goods and services
 
 neg_corr = ["Tomate"]
@@ -56,18 +58,23 @@ hline!(
     label = false
 )
 
+corr_ticks = -0.4:0.2:0.4
 
 acf(tsdata; lag=LAGS)
 p_acf_neg = current()
 hline!(p_acf_neg, [0], linewidth=2, color=:gray, alpha=0.5)
 xticks!(p_acf_neg, lags_ticks)
+ylims!(p_acf_neg, minimum(corr_ticks), maximum(corr_ticks))
+yticks!(p_acf_neg, corr_ticks)
 ylabel!(p_acf_neg, "Autocorrelation")
 xlabel!(p_acf_neg, "Lag")
 
 pacf(tsdata; type="real", lag=LAGS)
 p_pacf_neg = current()
 hline!(p_pacf_neg, [0], linewidth=2, color=:gray, alpha=0.5)
+ylims!(p_pacf_neg, ylims(p_acf_neg))
 xticks!(p_pacf_neg, lags_ticks)
+yticks!(p_pacf_neg, corr_ticks)
 ylabel!(p_pacf_neg, "Partial autocorrelation")
 xlabel!(p_pacf_neg, "Lag")
 
@@ -78,8 +85,6 @@ p_neg_corr = plot(
     layout=(1,3), 
     size=(800,300),
     guidefontsize=8,
-    leftmargin=3Plots.mm,
-    bottommargin=5Plots.mm,
 )
 
 ## Positive correlation goods and services
@@ -103,8 +108,6 @@ p2 = plot(
     dates, 
     tsdata,
     label="Lunch consumed outside",
-    # label=pos_corr[j], 
-    # label=fgt10.names[no_trans_2010[i]], 
     linewidth = 2, 
     guidefontsize = 8,
     xticks=(date_ticks, date_str),
@@ -121,9 +124,13 @@ hline!(
     label = false,
 )
 
+corr_ticks = -0.1:0.1:0.3
+
 acf(tsdata; lag=LAGS)
 p_acf_pos = current()
 hline!(p_acf_pos, [0], linewidth=2, color=:gray, alpha=0.5)
+ylims!(p_acf_pos, minimum(corr_ticks), maximum(corr_ticks))
+yticks!(p_acf_pos, corr_ticks)
 xticks!(p_acf_pos, lags_ticks)
 ylabel!(p_acf_pos, "Autocorrelation")
 xlabel!(p_acf_pos, "Lag")
@@ -131,9 +138,9 @@ xlabel!(p_acf_pos, "Lag")
 pacf(tsdata; type="real", lag=LAGS)
 p_pacf_pos = current()
 hline!(p_pacf_pos, [0], linewidth=2, color=:gray, alpha=0.5)
+ylims!(p_pacf_pos, ylims(p_acf_pos))
 xticks!(p_pacf_pos, lags_ticks)
-# yaxis!(p_pacf_pos, false)
-# yticks!(p_pacf_pos, (-0.2:0.1:0.3, ["" for _ in 1:6]))
+yticks!(p_pacf_pos, corr_ticks)
 ylabel!(p_pacf_pos, "Partial autocorrelation")
 xlabel!(p_pacf_pos, "Lag")
 
@@ -144,19 +151,21 @@ p_pos_corr = plot(
     layout=(1,3), 
     size=(800,300),
     guidefontsize=8,
-    leftmargin=3Plots.mm,
-    bottommargin=5Plots.mm,
 )
 
 
 
 ## Both plots: negative and extreme acf and pacf and good behaved with positive correlation  
 
-plot(
+corr_goods = plot(
     p_neg_corr, 
     p_pos_corr, 
     layout=(2,1),
-    size=(800,400)
+    size=(800,400),
+    leftmargin=2Plots.mm,
+    bottommargin=2Plots.mm,
 )
+
+display(corr_goods)
 
 savefig(joinpath(plots_savepath, "correlation_goods_services.pdf"))
