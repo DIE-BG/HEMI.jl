3eb3312771ebbedc06645c775c4c4c27ef4e40f5
Rodrigo Chang <rrcp777@gmail.com>
2022-05-24 15:09:43 -0600

    Actualización de paquetes MAY22

--- scripts/update_packages.jl
+++ scripts/update_packages.jl
@@ -5,13 +5,8 @@ import Pkg
 
 # Opción para actualizar los entornos
 UPDATE = true
-
-## Proyecto principal 
-@info "Actualización de paquetes del proyecto"
+# Directorio raíz del proyecto 
 ROOTDIR = projectdir()
-Pkg.activate(ROOTDIR)
-Pkg.status()
-UPDATE && Pkg.update()
 
 ## Paquetes internos 
 @info "Actualización de paquetes del paquete interno InflationEvalTools"
@@ -23,6 +18,12 @@ if UPDATE
     Pkg.resolve()
 end
 
+## Proyecto principal 
+@info "Actualización de paquetes del proyecto"
+Pkg.activate(ROOTDIR)
+Pkg.status()
+UPDATE && Pkg.update()
+
 ## Documentación 
 @info "Actualización de documentación"
 Pkg.activate(joinpath(ROOTDIR, "docs"))
@@ -32,3 +33,4 @@ UPDATE && Pkg.update()
 
 ## Activar el entorno del proyecto
 Pkg.activate(ROOTDIR)
+Pkg.resolve()
